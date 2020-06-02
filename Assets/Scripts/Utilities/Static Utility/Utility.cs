3333ca83ea0d48a458eed7239b60d2ba8ed3da6c
redd096 <martin.babino96@gmail.com>
2020-06-02 02:24:38 +0200

    aggiunto fade fill

--- Assets/Scripts/Utilities/Static Utility/Utility.cs
+++ Assets/Scripts/Utilities/Static Utility/Utility.cs
@@ -17,6 +17,15 @@
             image.color = new Color(image.color.r, image.color.g, image.color.b, alpha);
         }
 
+        static void Fade_Fill(this Image image, ref float delta, float from, float to, float duration)
+        {
+            //speed based to duration
+            delta += Time.deltaTime / duration;
+
+            //set fill amout
+            image.fillAmount = Mathf.Lerp(from, to, delta);
+        }
+
         #endregion
 
         /// <summary>
@@ -27,9 +36,8 @@
             Cursor.lockState = lockMode;
             Cursor.visible = lockMode != CursorLockMode.Locked;
         }
-
         /// <summary>
-        /// Use inside a coroutine, for fade in
+        /// Use inside a coroutine (delta 0 to 1), for fade in (alpha from 0 to 1)
         /// </summary>
         public static void FadeIn(this Image image, ref float delta, float duration)
         {
@@ -37,11 +45,27 @@
         }
 
         /// <summary>
-        /// Use inside a coroutine, for fade out
+        /// Use inside a coroutine (delta 0 to 1), for fade out (alpha from 1 to 0)
         /// </summary>
         public static void FadeOut(this Image image, ref float delta, float duration)
         {
             image.Fade(ref delta, 1, 0, duration);
+        }
+
+        /// <summary>
+        /// Use inside a coroutine (delta 0 to 1), for fade in (alpha from 0 to 1) with fillAmount
+        /// </summary>
+        public static void FadeIn_Fill(this Image image, ref float delta, float duration)
+        {
+            image.Fade_Fill(ref delta, 0, 1, duration);
+        }
+
+        /// <summary>
+        /// Use inside a coroutine (delta 0 to 1), for fade out (alpha from 1 to 0) with fillAmount
+        /// </summary>
+        public static void FadeOut_Fill(this Image image, ref float delta, float duration)
+        {
+            image.Fade_Fill(ref delta, 1, 0, duration);
         }
     }
 }
