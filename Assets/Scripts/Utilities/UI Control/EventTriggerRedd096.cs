361e683c6172d3ed14fff858afac6c3508c98216
redd096 <martin.babino96@gmail.com>
2020-06-02 15:01:56 +0200

    ColorValue spostato da event trigger a un file a parte

--- Assets/Scripts/Utilities/UI Control/EventTriggerRedd096.cs
+++ Assets/Scripts/Utilities/UI Control/EventTriggerRedd096.cs
@@ -44,12 +44,4 @@
             GetComponent<Image>().color = colorValue.Color;
         }
     }
-
-    [CreateAssetMenu(fileName = "Color", menuName = "redd096/UIControl/Color")]
-    public class ColorValue : ScriptableObject
-    {
-        [SerializeField] Color color = Color.red;
-
-        public Color Color { get { return color; } }
-    }
 }
