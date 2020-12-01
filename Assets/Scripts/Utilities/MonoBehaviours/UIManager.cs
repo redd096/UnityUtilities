79bbea4e5b03e48381e53b61d9a9ba8c5efabf7f
redd096 <martin.babino96@gmail.com>
2020-12-01 12:33:13 +0100

    migliorato UI Manager

--- Assets/Scripts/Utilities/MonoBehaviours/UIManager.cs
+++ Assets/Scripts/Utilities/MonoBehaviours/UIManager.cs
@@ -1,14 +1,24 @@
 ﻿namespace redd096
 {
     using UnityEngine;
+    using UnityEngine.UI;
 
     [AddComponentMenu("redd096/MonoBehaviours/UI Manager")]
     public class UIManager : MonoBehaviour
     {
         [SerializeField] GameObject pauseMenu = default;
 
+        void Start()
+        {
+            PauseMenu(false);
+        }
+
         public void PauseMenu(bool active)
         {
+            if (pauseMenu == null)
+                return;
+
+            //active or deactive pause menu
             pauseMenu.SetActive(active);
         }
     }
