a97031e12c6be3f3581cb3778329e25808788c03
redd096 <martin.babino96@gmail.com>
2021-02-05 14:26:27 +0100

    inserito attributo ReadOnly

---
+++ Assets/Scripts/Utilities/Attributes/ReadOnlyAttribute.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+ [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    //public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    //{
+    //    return EditorGUI.GetPropertyHeight(property, label, true);
+    //}
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        //disable GUI for this property
+        GUI.enabled = false;
+
+        EditorGUI.PropertyField(position, property, label, true);
+
+        //re-enable GUI
+        GUI.enabled = true;
+    }
+}
+
+#endif
+
+//attribute
+public class ReadOnlyAttribute : PropertyAttribute
+{
+}
