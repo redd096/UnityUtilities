1b25685ae6d56e2396f5a1287e5e05e763729c5f
redd096 <martin.babino96@gmail.com>
2021-02-07 12:47:53 +0100

    rimosso CanvasFaceCamera, che faceva lo stesso di LookCamera

--- Assets/Scripts/redd096/MonoBehaviours/UIManager.cs
+++ Assets/Scripts/redd096/MonoBehaviours/UIManager.cs
@@ -6,17 +6,22 @@
     [AddComponentMenu("redd096/MonoBehaviours/UI Manager")]
     public class UIManager : MonoBehaviour
     {
+        [Header("Menu")]
         [SerializeField] GameObject pauseMenu = default;
 
         void Start()
         {
+            //by default, deactive pause menu
             PauseMenu(false);
         }
 
         public void PauseMenu(bool active)
         {
             if (pauseMenu == null)
+            {
+                Debug.LogWarning("There is no pause menu");
                 return;
+            }
 
             //active or deactive pause menu
             pauseMenu.SetActive(active);
