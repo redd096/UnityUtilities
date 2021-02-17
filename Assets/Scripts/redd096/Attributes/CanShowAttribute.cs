366605ea752c54021391bbbf7fd981f044916789
redd096 <martin.babino96@gmail.com>
2021-02-17 11:04:47 +0100

    fix CanShowAttribute che non faceva buildare

--- Assets/Scripts/redd096/Attributes/CanShowAttribute.cs
+++ Assets/Scripts/redd096/Attributes/CanShowAttribute.cs
@@ -12,8 +12,7 @@
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
             //update height only if can show (to hide space where there are hidden variables) - necessary also to update height when open a list/array/struct in inspector
-            CanShowAttribute canShowAttribute = attribute as CanShowAttribute;
-            if (canShowAttribute.CanShow(property))
+            if (CanShow(property))
             {
                 return EditorGUI.GetPropertyHeight(property, label, true);
             }
@@ -25,11 +24,39 @@
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             //show only if CanShow return true
-            CanShowAttribute canShowAttribute = attribute as CanShowAttribute;
-            if (canShowAttribute.CanShow(property))
+            if (CanShow(property))
             {
                 EditorGUI.PropertyField(position, property, label, true);
             }
+        }
+
+        bool CanShow(SerializedProperty property)
+        {
+            CanShowAttribute canShowAttribute = attribute as CanShowAttribute;
+
+            //foreach value to check
+            foreach (string value in canShowAttribute.values)
+            {
+                bool check = property.serializedObject.FindProperty(value).boolValue;
+
+                //use NOT
+                if (canShowAttribute.NOT)
+                    check = !check;
+
+                //if checkAND, when find one false, return false
+                if (canShowAttribute.checkAND && !check)
+                {
+                    return false;
+                }
+                //if checkOR, when find one true, return true
+                else if (!canShowAttribute.checkAND && check)
+                {
+                    return true;
+                }
+            }
+
+            //else return checkAND, cause it mean every value is true if checkAND, or every value is false if checkOR
+            return canShowAttribute.checkAND;
         }
     }
 
@@ -48,7 +75,7 @@
         /// </summary>
         public bool NOT = false;
 
-        string[] values;
+        public readonly string[] values;
 
         /// <summary>
         /// Attribute to show variable only if specific values are true
@@ -57,33 +84,6 @@
         public CanShowAttribute(params string[] values)
         {
             this.values = values;
-        }
-
-        public bool CanShow(SerializedProperty property)
-        {
-            //foreach value to check
-            foreach (string value in values)
-            {
-                bool check = property.serializedObject.FindProperty(value).boolValue;
-
-                //use NOT
-                if (NOT)
-                    check = !check;
-
-                //if checkAND, when find one false, return false
-                if (checkAND && !check)
-                {
-                    return false;
-                }
-                //if checkOR, when find one true, return true
-                else if (!checkAND && check)
-                {
-                    return true;
-                }
-            }
-
-            //else return checkAND, cause it mean every value is true if checkAND, or every value is false if checkOR
-            return checkAND;
         }
     }
 }
