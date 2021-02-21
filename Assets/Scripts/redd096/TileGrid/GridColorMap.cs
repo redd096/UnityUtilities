46cba80372e30bf474a4ff836bc207015278df41
redd096 <martin.babino96@gmail.com>
2021-02-21 12:17:58 +0100

    tileGrid inserito tasto per rimuovere la grid

--- Assets/Scripts/redd096/TileGrid/GridColorMap.cs
+++ Assets/Scripts/redd096/TileGrid/GridColorMap.cs
@@ -20,7 +20,15 @@
                 ((GridColorMap)target).RegenGrid();
 
                 //set undo
-                Undo.RegisterFullObjectHierarchyUndo(target, "Regen World");
+                Undo.RegisterFullObjectHierarchyUndo(target, "Regen Grid");
+            }
+
+            if(GUILayout.Button("Destroy Grid"))
+            {
+                ((GridColorMap)target).DestroyGrid();
+
+                //set undo
+                Undo.RegisterFullObjectHierarchyUndo(target, "Destroy Grid");
             }
         }
     }
@@ -54,6 +62,12 @@
             //remove old grid and generate new one
             RemoveOldGrid();
             GenerateGrid(gridImage.width, gridImage.height);
+        }
+
+        public void DestroyGrid()
+        {
+            //remove old grid
+            RemoveOldGrid();
         }
     }
 }
