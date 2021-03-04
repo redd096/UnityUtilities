12a3f7d1d8c53041fc9170485cf2e13d9f4f84b6
redd096 <martin.babino96@gmail.com>
2021-03-04 21:44:02 +0100

    creato input manager per utilizzare il nuovo input system come fosse il vecchio

---
+++ Assets/Scripts/redd096/Singletons/InputManagerRedd096.cs
@@ -0,0 +1,260 @@
+﻿namespace redd096
+{
+    using UnityEngine;
+    using UnityEngine.InputSystem;
+
+    [AddComponentMenu("redd096/Singletons/Input Manager redd096")]
+    [RequireComponent(typeof(PlayerInput))]
+    public class InputManagerRedd096 : Singleton<InputManagerRedd096>
+    {
+        [Header("Input Action Asset")]
+        public InputActionAsset inputActionAsset;
+
+        public PlayerInput playerInput { get; set; }
+
+        void OnEnable()
+        {
+            //get PlayerInput and be sure has actions setted
+            playerInput = GetComponent<PlayerInput>();
+            if (playerInput.actions != inputActionAsset)
+                playerInput.actions = inputActionAsset;
+        }
+    }
+
+    public static class InputRedd096
+    {
+        #region using input actions asset
+
+        /// <summary>
+        /// Returns true during the frame the user pressed down the virtual button identified by inputName
+        /// </summary>
+        public static bool GetButtonDown(string inputName)
+        {
+            return InputManagerRedd096.instance.inputActionAsset.FindAction(inputName).triggered;
+        }
+
+        /// <summary>
+        /// Returns true while the virtual button identified by inputName is held down
+        /// </summary>
+        public static bool GetButton(string inputName)
+        {
+            return InputManagerRedd096.instance.inputActionAsset.FindAction(inputName).phase == InputActionPhase.Started;
+        }
+
+        /// <summary>
+        /// Returns the value of the virtual T identified by inputtName (GetAxis returns only float, this one returns every type of value)
+        /// </summary>
+        public static T GetValue<T>(string inputName) where T : struct
+        {
+            return InputManagerRedd096.instance.inputActionAsset.FindAction(inputName).ReadValue<T>();
+        }
+
+        #endregion
+
+        #region compare
+
+        /// <summary>
+        /// Returns true if current activeControl is the same on inputName1 and inputName2 
+        /// </summary>
+        public static bool IsSameInput(string inputName1, string inputName2)
+        {
+            return InputManagerRedd096.instance.inputActionAsset.FindAction(inputName1).activeControl.name == InputManagerRedd096.instance.inputActionAsset.FindAction(inputName2).activeControl.name;
+        }
+
+        /// <summary>
+        /// Returns true if current activeControl in inputName1 is "escape"
+        /// </summary>
+        public static bool IsEscape(string inputName)
+        {
+            return InputManagerRedd096.instance.inputActionAsset.FindAction(inputName).activeControl.name == "escape";
+        }
+
+        /// <summary>
+        /// Returns true if controlSchemeName is current control scheme
+        /// </summary>
+        public static bool IsCurrentControlScheme(string controlSchemeName)
+        {
+            //return InputManagerRedd096.instance.playerInput.currentControlScheme == InputManagerRedd096.instance.inputActionAsset.FindControlScheme(controlSchemeName).Value.name
+            return InputManagerRedd096.instance.playerInput.currentControlScheme == controlSchemeName;
+        }
+
+        #endregion
+
+        #region replace old input system
+
+        /// <summary>
+        /// Returns the value of the virtual axis identified by inputtName
+        /// </summary>
+        public static float GetAxis(string inputName)
+        {
+            return InputManagerRedd096.instance.inputActionAsset.FindAction(inputName).ReadValue<float>();
+        }
+
+        /// <summary>
+        /// Return TouchState struct (with position, phase and much more)
+        /// </summary>
+        public static UnityEngine.InputSystem.LowLevel.TouchState GetTouch(int index)
+        {
+            return Touchscreen.current.touches[index].ReadValue();
+        }
+
+        #region get key
+
+        /// <summary>
+        /// Returns true during the frame the user pressed down the key identified by the Key enum parameter
+        /// </summary>
+        public static bool GetKeyDown(Key key)
+        {
+            return Keyboard.current[key].wasPressedThisFrame;
+        }
+
+        /// <summary>
+        /// Returns true while the user holds down the key identified by the Key enum parameter
+        /// </summary>
+        public static bool GetKey(Key key)
+        {
+            return Keyboard.current[key].isPressed;
+        }
+
+        /// <summary>
+        /// Returns true during the frame the user releases the key identified by the Key enum parameter
+        /// </summary>
+        public static bool GetKeyUp(Key key)
+        {
+            return Keyboard.current[key].wasReleasedThisFrame;
+        }
+
+        /// <summary>
+        /// Returns true during the frame the user pressed down the key identified by the GamepadButton enum parameter
+        /// </summary>
+        public static bool GetKeyDown(UnityEngine.InputSystem.LowLevel.GamepadButton key)
+        {
+            return Gamepad.current[key].wasPressedThisFrame;
+        }
+
+        /// <summary>
+        /// Returns true while the user holds down the key identified by the GamepadButton enum parameter
+        /// </summary>
+        public static bool GetKey(UnityEngine.InputSystem.LowLevel.GamepadButton key)
+        {
+            return Gamepad.current[key].isPressed;
+        }
+
+        /// <summary>
+        /// Returns true during the frame the user releases the key identified by the GamepadButton enum parameter
+        /// </summary>
+        public static bool GetKeyUp(UnityEngine.InputSystem.LowLevel.GamepadButton key)
+        {
+            return Gamepad.current[key].wasReleasedThisFrame;
+        }
+
+        #endregion
+
+        #region get mouse button
+
+        /// <summary>
+        /// Returns true during the frame the user pressed down the left mouse button
+        /// </summary>
+        public static bool GetLeftMouseButtonDown()
+        {
+            return Mouse.current.leftButton.wasPressedThisFrame;
+        }
+
+        /// <summary>
+        /// Returns true while the user holds down the left mouse button
+        /// </summary>
+        public static bool GetLeftMouseButton()
+        {
+            return Mouse.current.leftButton.isPressed;
+        }
+
+        /// <summary>
+        /// Returns true during the frame the user releases the left mouse button
+        /// </summary>
+        public static bool GetLeftMouseButtonUp()
+        {
+            return Mouse.current.leftButton.wasReleasedThisFrame;
+        }
+
+        /// <summary>
+        /// Returns true during the frame the user pressed down the right mouse button
+        /// </summary>
+        public static bool GetRightMouseButtonDown()
+        {
+            return Mouse.current.rightButton.wasPressedThisFrame;
+        }
+
+        /// <summary>
+        /// Returns true while the user holds down the right mouse button
+        /// </summary>
+        public static bool GetRightMouseButton()
+        {
+            return Mouse.current.rightButton.isPressed;
+        }
+
+        /// <summary>
+        /// Returns true during the frame the user releases the right mouse button
+        /// </summary>
+        public static bool GetRightMouseButtonUp()
+        {
+            return Mouse.current.rightButton.wasReleasedThisFrame;
+        }
+
+        /// <summary>
+        /// Returns true during the frame the user pressed down the middle mouse button
+        /// </summary>
+        public static bool GetMiddleMouseButtonDown()
+        {
+            return Mouse.current.middleButton.wasPressedThisFrame;
+        }
+
+        /// <summary>
+        /// Returns true while the user holds down the middle mouse button
+        /// </summary>
+        public static bool GetMiddleMouseButton()
+        {
+            return Mouse.current.middleButton.isPressed;
+        }
+
+        /// <summary>
+        /// Returns true during the frame the user releases the middle mouse button
+        /// </summary>
+        public static bool GetMiddleMouseButtonUp()
+        {
+            return Mouse.current.middleButton.wasReleasedThisFrame;
+        }
+
+        #endregion
+
+        #region general
+
+        /// <summary>
+        /// Last measured linear acceleration of a device in three-dimensional space
+        /// </summary>
+        public static Vector3 acceleration => Accelerometer.current.acceleration.ReadValue();
+        /// <summary>
+        /// Is any key or mouse button currenttly held down?
+        /// </summary>
+        public static bool anyKey => Keyboard.current.anyKey.isPressed || Mouse.current.leftButton.isPressed || Mouse.current.rightButton.isPressed || Mouse.current.middleButton.isPressed;
+        /// <summary>
+        /// Returns true the first frame the user hits any key or mouse button
+        /// </summary>
+        public static bool anyKeyDown => Keyboard.current.anyKey.wasPressedThisFrame || Mouse.current.leftButton.wasPressedThisFrame || Mouse.current.rightButton.wasPressedThisFrame || Mouse.current.middleButton.wasPressedThisFrame;
+        /// <summary>
+        /// Returns the attitude (ie, orientation in space) of the device
+        /// </summary>
+        public static Quaternion gyroAttitude => AttitudeSensor.current.attitude.ReadValue();
+        /// <summary>
+        /// The current mouse position in pixel coordinates
+        /// </summary>
+        public static Vector2 mousePosition => Mouse.current.position.ReadValue();
+        /// <summary>
+        /// Number of touches
+        /// </summary>
+        public static int touchCount => Touchscreen.current.touches.Count;
+
+        #endregion
+
+        #endregion
+    }
+}
