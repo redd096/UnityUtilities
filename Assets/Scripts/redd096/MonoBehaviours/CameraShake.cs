f4035a92b721a6951511c47c8da1d8265098e7cb
redd096 <martin.babino96@gmail.com>
2021-03-24 14:12:18 +0100

    inserito overwrite camera shake + camera shake settabile da script

--- Assets/Scripts/redd096/MonoBehaviours/CameraShake.cs
+++ Assets/Scripts/redd096/MonoBehaviours/CameraShake.cs
@@ -6,12 +6,15 @@
 	[AddComponentMenu("redd096/MonoBehaviours/Camera Shake")]
 	public class CameraShake : MonoBehaviour
 	{
-		[Header("Camera - by default is this transform")]
-		[Tooltip("If null, use this transform")] [SerializeField] Transform camTransform;
+		[Header("Camera - by default is main camera")]
+		[Tooltip("If null, use main camera")] [SerializeField] Transform camTransform;
 
 		[Header("Shake")]
 		[Tooltip("How long should shake")] [SerializeField] float shakeDuration = 1f;
 		[Tooltip("Amplitude of the shake. A larger value shakes the camera harder")] [SerializeField] float shakeAmount = 0.7f;
+
+		[Header("Overwrite - start shake also if another is running")]
+		[Tooltip("If another shake is already running, stop it and start new one")] [SerializeField] bool ovewriteShake = true;
 
 		Coroutine shakeCoroutine;
 		Vector3 originalPos;
@@ -21,37 +24,58 @@
 			//get reference if null
 			if (camTransform == null)
 			{
-				camTransform = transform;
+				camTransform = Camera.main.transform;
 			}
 
 			//then save original position
 			originalPos = camTransform.localPosition;
 		}
 
-		IEnumerator ShakeCoroutine()
+		IEnumerator ShakeCoroutine(float duration, float amount)
 		{
 			//set shake duration
-			float shake = Time.time + shakeDuration;
+			float shake = Time.time + duration;
 
-			//shake for shakeDuration
+			//shake
 			while (shake > Time.time)
 			{
-				camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+				camTransform.localPosition = originalPos + Random.insideUnitSphere * amount;
 
 				yield return null;
 			}
 
 			//then reset to original position
 			camTransform.localPosition = originalPos;
+
+			shakeCoroutine = null;
 		}
 
-		public void StartShake()
+        #region public API
+
+		/// <summary>
+		/// Start shake, using variables in inspector
+		/// </summary>
+        public void StartShake()
 		{
-			//start or restart shake coroutine
-			if (shakeCoroutine != null)
-				StopCoroutine(shakeCoroutine);
-
-			shakeCoroutine = StartCoroutine(ShakeCoroutine());
+			StartShake(shakeDuration, shakeAmount);
 		}
-	}
+
+		/// <summary>
+		/// Start shake, using specific duration and amount
+		/// </summary>
+		public void StartShake(float duration, float amount)
+		{
+			//do only if there is not another shake, or can overwrite it
+			if (shakeCoroutine == null || ovewriteShake)
+			{
+				//start or restart shake coroutine
+				if (shakeCoroutine != null)
+					StopCoroutine(shakeCoroutine);
+
+				shakeCoroutine = StartCoroutine(ShakeCoroutine(duration, amount));
+			}
+		}
+
+        #endregion
+    }
 }
