a3399d098db31e231344a263184893b658a6495b
redd096 <martin.babino96@gmail.com>
2021-09-19 10:54:45 +0200

    rimossi script visual state machine

--- Assets/Scripts/redd096/Visual State Machine/StateBox.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class StateBox
-{
-    //TODO
-    //serviranno le posizioni, i trigger a cui sono collegati, gli stati e substati che hanno
-}
