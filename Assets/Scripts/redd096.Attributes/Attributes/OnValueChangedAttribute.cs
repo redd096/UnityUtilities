8911b0ceca445190924ad4989d0b175f6de2cead
redd096 <martin.babino96@gmail.com>
2022-01-18 19:20:29 +0100

    attributo per chiamare una funzione quando viene modificato un valore

---
+++ Assets/Scripts/redd096.Attributes/Attributes/OnValueChangedAttribute.cs
@@ -0,0 +1,72 @@
+﻿using System.Reflection;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace redd096.Attributes
+{
+    #region editor
+
+#if UNITY_EDITOR
+
+    [CustomPropertyDrawer(typeof(OnValueChangedAttribute))]
+    public class OnChangedCallAttributePropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            //draw property
+            EditorGUI.BeginChangeCheck();
+            EditorGUI.PropertyField(position, property);
+
+            //if changed something
+            if (EditorGUI.EndChangeCheck())
+            {
+                OnValueChangedAttribute at = attribute as OnValueChangedAttribute;
+
+                //in target object, find method with same name and invoke
+                foreach(MethodInfo method in property.serializedObject.targetObject.GetType().GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.DeclaredOnly))
+                {
+                    if(method.Name == at.methodName)
+                    {
+                        //only if with 0 parameters
+                        if (method != null && method.GetParameters().Length == 0)
+                        {
+                            method.Invoke(property.serializedObject.targetObject, null);
+
+                            //repaint scene
+                            SceneView.RepaintAll();
+                        }
+                        else
+                        {
+                            Debug.LogWarning(at.GetType().Name + "can invoke only methods with 0 parameters", property.serializedObject.targetObject);
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+#endif
+
+    #endregion
+
+    /// <summary>
+    /// Attribute to call a method when value is changed
+    /// </summary>
+    public class OnValueChangedAttribute : PropertyAttribute
+    {
+        public string methodName;
+
+        /// <summary>
+        /// Attribute to call a method when value is changed
+        /// </summary>
+        /// <param name="methodNameNoArguments"></param>
+        public OnValueChangedAttribute(string methodNameNoArguments)
+        {
+            methodName = methodNameNoArguments;
+        }
+    }
+}
