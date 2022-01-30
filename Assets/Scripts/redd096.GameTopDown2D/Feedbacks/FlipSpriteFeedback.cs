e58d061ffcbf12d79a5bc000a9941dcdd9a90a27
redd096 <martin.babino96@gmail.com>
2022-01-30 13:25:07 +0100

    TEMP flip sprite fix for enemies with new path finding

--- Assets/Scripts/redd096.GameTopDown2D/Feedbacks/FlipSpriteFeedback.cs
+++ Assets/Scripts/redd096.GameTopDown2D/Feedbacks/FlipSpriteFeedback.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace redd096.GameTopDown2D
 {
@@ -12,8 +13,15 @@ namespace redd096.GameTopDown2D
         [Tooltip("By default these sprites are looking to the right?")] [SerializeField] bool defaultLookRight = true;
         [SerializeField] SpriteRenderer[] spritesToFlip = default;
 
+        //TEMP
+        Coroutine flipSpriteCoroutine;
+        Character selfCharacter;
+
         void OnEnable()
         {
+            //TEMP
+            selfCharacter = GetComponentInParent<Character>();
+
             //get references
             if (aimComponent == null) aimComponent = GetComponentInParent<AimComponent>();
             if (spritesToFlip == null || spritesToFlip.Length <= 0) spritesToFlip = GetComponentsInChildren<SpriteRenderer>();
@@ -22,7 +30,7 @@ namespace redd096.GameTopDown2D
             if (aimComponent)
             {
                 aimComponent.onChangeAimDirection += OnChangeAimDirection;
-                OnChangeAimDirection(aimComponent.IsLookingRight);     //set default rotation
+                OnChangeAimDirection(aimComponent.IsLookingRight);      //set default rotation
             }
         }
 
@@ -35,10 +43,34 @@ namespace redd096.GameTopDown2D
 
         void OnChangeAimDirection(bool isLookingRight)
         {
+            //TEMP for enemies use a coroutine
+            if (selfCharacter && selfCharacter.CharacterType == Character.ECharacterType.AI)
+            {
+                //restart coroutine
+                if (flipSpriteCoroutine != null)
+                    StopCoroutine(flipSpriteCoroutine);
+
+                flipSpriteCoroutine = StartCoroutine(FlipSpriteCoroutine());
+                return;
+            }
+
             //flip right or left
             foreach (SpriteRenderer sprite in spritesToFlip)
                 if (sprite)
                     sprite.flipX = (defaultLookRight && isLookingRight == false) || (defaultLookRight == false && isLookingRight);
+        }
+
+        //TEMP - is used from enemies because now they receive path after few frames and can have rotation problems
+        IEnumerator FlipSpriteCoroutine()
+        {
+            //wait a bit before rotate
+            yield return new WaitForSeconds(0.1f);
+
+            //flip right or left
+            if (aimComponent)
+                foreach (SpriteRenderer sprite in spritesToFlip)
+                    if (sprite)
+                        sprite.flipX = (defaultLookRight && aimComponent.IsLookingRight == false) || (defaultLookRight == false && aimComponent.IsLookingRight);
         }
     }
 }
