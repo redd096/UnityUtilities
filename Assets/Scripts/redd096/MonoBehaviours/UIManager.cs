97e22fa60bc5b18e394031766353c03d7c5a941c
redd096 <martin.babino96@gmail.com>
2022-03-13 13:11:53 +0100

    UIManager aggiunto input delay quando si aprono i menu

--- Assets/Scripts/redd096/MonoBehaviours/UIManager.cs
+++ Assets/Scripts/redd096/MonoBehaviours/UIManager.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 namespace redd096
 {
@@ -7,27 +9,64 @@ namespace redd096
     public class UIManager : MonoBehaviour
     {
         [Header("Menu")]
+        [SerializeField] float delayInputWhenOpenMenu = 0.3f;
         [SerializeField] GameObject pauseMenu = default;
         [SerializeField] GameObject endMenu = default;
 
+        //delay input when open menu
+        EventSystem eventSystem;
+        Coroutine delayInputCoroutine;
+
         void Start()
         {
+            //get references
+            eventSystem = EventSystem.current;
+
             //by default, deactive menus
             PauseMenu(false);
             EndMenu(false);
         }
 
+        #region open menu with input delay
+
+        void OpenMenu(GameObject menu, bool active)
+        {
+            if (menu == null)
+                return;
+
+            //when active menu, deactive event system for a little time
+            if (active)
+            {
+                if (eventSystem) eventSystem.enabled = false;
+
+                //restart coroutine
+                if (delayInputCoroutine != null) StopCoroutine(delayInputCoroutine);
+                delayInputCoroutine = StartCoroutine(DelayInputCoroutine());
+            }
+
+            //active or deactive menu
+            menu.SetActive(active);
+        }
+
+        IEnumerator DelayInputCoroutine()
+        {
+            //wait (real time, so if Time.timeScale is 0 it works anyway)
+            float timeDelay = Time.realtimeSinceStartup + delayInputWhenOpenMenu;
+            while (Time.realtimeSinceStartup < timeDelay)
+                yield return null;
+
+            //then re-enable event system
+            if (eventSystem) eventSystem.enabled = true;
+        }
+
+        #endregion
+
         #region menu
 
         public void PauseMenu(bool active)
         {
-            if (pauseMenu == null)
-            {
-                return;
-            }
-
             //active or deactive pause menu
-            pauseMenu.SetActive(active);
+            OpenMenu(pauseMenu, active);
         }
 
         public void EndMenu(bool active)
@@ -41,8 +80,8 @@ namespace redd096
             if (active)
                 PauseMenu(false);
 
-            //active or deactive pause menu
-            endMenu.SetActive(active);
+            //active or deactive end menu
+            OpenMenu(endMenu, active);
         }
 
         #endregion
