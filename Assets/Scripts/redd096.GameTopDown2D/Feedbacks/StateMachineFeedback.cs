d502a8035ee49b21bfca46701d387b9db315d5da
redd096 <martin.babino96@gmail.com>
2022-04-12 12:05:58 +0200

    inserito feedback quando si entra in uno stato

---
+++ Assets/Scripts/redd096.GameTopDown2D/Feedbacks/StateMachineFeedback.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+namespace redd096.GameTopDown2D
+{
+    [AddComponentMenu("redd096/.GameTopDown2D/Feedbacks/StateMachine Feedback")]
+    public class StateMachineFeedback : MonoBehaviour
+    {
+        [Header("Necessary Components - default get in child and parent")]
+        [SerializeField] Animator anim = default;
+        [SerializeField] StateMachineRedd096 stateMachine = default;
+
+        [Header("States and triggers in animator")]
+        [SerializeField] AnimTriggerStateStruct[] animTriggers = default;
+
+        void OnEnable()
+        {
+            //get references
+            if (anim == null) anim = GetComponentInChildren<Animator>();
+            if (stateMachine == null)
+            {
+                Redd096Main main = GetComponentInParent<Redd096Main>();
+                if (main) stateMachine = main.GetComponentInChildren<StateMachineRedd096>();
+            }
+
+            //add events
+            if (stateMachine)
+            {
+                stateMachine.onSetState += OnSetState;
+            }
+        }
+
+        void OnDisable()
+        {
+            //remove events
+            if (stateMachine)
+            {
+                stateMachine.onSetState -= OnSetState;
+            }
+        }
+
+        void OnSetState(string stateName)
+        {
+            foreach (AnimTriggerStateStruct animTrigger in animTriggers)
+            {
+                //if enter in this state
+                if (stateName.Equals(animTrigger.State))
+                {
+                    //set trigger
+                    if (anim)
+                        anim.SetTrigger(animTrigger.TriggerToSet);
+
+                    break;
+                }
+            }
+        }
+    }
+
+    [System.Serializable]
+    public struct AnimTriggerStateStruct
+    {
+        public string State;
+        public string TriggerToSet;
+    }
+}
