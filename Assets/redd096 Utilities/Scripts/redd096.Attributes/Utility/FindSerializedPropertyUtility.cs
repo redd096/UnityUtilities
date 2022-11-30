7db615158778012f3009893a439ad094c9cac9ef
redd096 <martin.babino96@gmail.com>
2022-12-01 00:02:14 +0100

    reinseriti metodi per ottenere SerializedProperty

--- Assets/redd096 Utilities/Scripts/redd096.Attributes/Utility/FindSerializedPropertyUtility.cs
+++ Assets/redd096 Utilities/Scripts/redd096.Attributes/Utility/FindSerializedPropertyUtility.cs
@@ -13,39 +13,6 @@ namespace redd096.Attributes
 
         #region find target object
 
-        ///// <summary>
-        ///// Gets the object the property represents.
-        ///// </summary>
-        ///// <param name="property"></param>
-        ///// <returns></returns>
-        //public static object GetTargetObjectOfProperty(this SerializedProperty property)
-        //{
-        //    if (property == null)
-        //    {
-        //        return null;
-        //    }
-        //
-        //    string path = property.propertyPath.Replace(".Array.data[", "[");
-        //    object obj = property.serializedObject.targetObject;
-        //    string[] elements = path.Split('.');
-        //
-        //    foreach (var element in elements)
-        //    {
-        //        if (element.Contains("["))
-        //        {
-        //            string elementName = element.Substring(0, element.IndexOf("["));
-        //            int index = Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", "").Replace("]", ""));
-        //            obj = GetValue_Imp(obj, elementName, index);
-        //        }
-        //        else
-        //        {
-        //            obj = GetValue_Imp(obj, element);
-        //        }
-        //    }
-        //
-        //    return obj;
-        //}
-
         /// <summary>
         /// Gets the object that the property is a member of
         /// </summary>
@@ -126,84 +93,139 @@ namespace redd096.Attributes
 
         #endregion
 
-        #region find correct property
+        #region find property
 
-        ///// <summary>
-        ///// Find property also if inside an array
-        ///// </summary>
-        ///// <param name="property"></param>
-        ///// <param name="path"></param>
-        ///// <returns></returns>
-        //public static SerializedProperty FindCorrectProperty(this SerializedProperty property, string path)
-        //{
-        //    //if in array, get parent and find property in it
-        //    SerializedProperty parent = GetParent(property);
-        //    if (parent != null)
-        //    {
-        //        return parent.FindPropertyRelative(path);
-        //    }
-        //
-        //    //else find property normally
-        //    return property.serializedObject.FindProperty(path);
-        //}
-        //
-        ///// <summary>
-        ///// Return parent inside array. From this can return its properties calling FindPropertyRelative(path)
-        ///// </summary>
-        ///// <param name="property"></param>
-        ///// <returns></returns>
-        //public static SerializedProperty GetParent(this SerializedProperty property)
-        //{
-        //    //if in array, path will be propertyName.Array.data[index].VariableName (where VariableName is the name of the var you are looking for)
-        //    string path = property.propertyPath;
-        //    int i = path.LastIndexOf('.');
-        //
-        //    //if there is no point, is not an array
-        //    if (i < 0)
-        //        return null;
-        //
-        //    //if finish with "Array" without data[index], then this property is already an array, find a property inside this
-        //    path = path.Substring(0, i);
-        //    if (path.Substring(i - 5, 5) == "Array")
-        //        return property;
-        //
-        //    //else find the property containing variableName
-        //    return property.serializedObject.FindProperty(path);
-        //}
-        //
-        ///// <summary>
-        ///// Return parent of this array
-        ///// </summary>
-        ///// <param name="property"></param>
-        ///// <returns></returns>
-        //public static SerializedProperty GetArrayParent(this SerializedProperty property)
-        //{
-        //    //if in array, path will be parentName.Array.data[index].PropertyName (where PropertyName is the name of this property)
-        //    string path = property.propertyPath;
-        //    int i = path.LastIndexOf('.');
-        //
-        //    //if there is no point, is not an array
-        //    if (i < 0)
-        //        return null;
-        //
-        //    //move back for two points (between "Array.data" and between "parentName.Array")
-        //    int index;
-        //    int j = 0;
-        //    for (index = i - 1; index >= 0; index--)
-        //    {
-        //        if (path[index] == '.')
-        //        {
-        //            j++;
-        //
-        //            if (j == 2)
-        //                break;
-        //        }
-        //    }
-        //
-        //    //get the path to parent and return it
-        //    path = path.Substring(0, index);
-        //    return property.serializedObject.FindProperty(path);
-        //}
+        /// <summary>
+        /// Find property also if inside an array or in parent
+        /// </summary>
+        /// <param name="property"></param>
+        /// <param name="propertyToFind"></param>
+        /// <returns></returns>
+        public static SerializedProperty FindCorrectProperty(this SerializedProperty property, string propertyToFind)
+        {
+            //if parent is null, path is: propertyName
+            //if property is array, path is: propertyName.Array.data[index] (because property drawer calculate every element of array as single property)
+            //if property has a parent, path is: parentName.propertyName
+            //if parent is array, path is: parentName.Array.data[index].propertyName
+
+            //if propertyToFind is in same object of this property, call: property.serializedObject.FindProperty(propertyToFind)
+            //if this property is an array and propertyToFind is an element inside of this array, call: property.FindPropertyRelative(propertyToFind)
+            //if this property is a struct or class and propertyToFind is an element inside of it, call: property.FindPropertyRelative(propertyToFind)
+            //if this property is inside a struct and propertyToFind is in same object of the struct, call: property.GetParent().serializedObject.FindProperty(propertyToFind)
+
+            //try get property inside of this object
+            SerializedProperty foundProperty = property.serializedObject.FindProperty(propertyToFind);
+            if (foundProperty != null) 
+                return foundProperty;
+
+            //else try to find inside of this property (if it's a struct, a class or array and propertyToFind is inside of it)
+            foundProperty = property.FindPropertyRelative(propertyToFind);
+            if (foundProperty != null)
+                return foundProperty;
+
+            //else try to find inside every parent
+            SerializedProperty parent = property.GetPropertyParent();
+            while (foundProperty == null && parent != null)
+            {
+                foundProperty = parent.serializedObject.FindProperty(propertyToFind);
+                parent = property.GetPropertyParent();
+            }
+
+            return foundProperty;
+        }
+        
+        /// <summary>
+        /// Return parent of this property
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        public static SerializedProperty GetPropertyParent(this SerializedProperty property)
+        {
+            //if in array, path will be
+            //parentName.Array.data[index].property
+            //but if this property is an array too, path will be
+            //parentName.Array.data[index].property.Array.data[index]
+
+            //remove every .Array.data so we have parentName[index].property (or parentName[index].property[index])
+            string path = property.propertyPath.Replace(".Array.data", "");
+            int i = path.LastIndexOf('.');
+
+            //if there isn't, there is not a parent
+            if (i < 0)
+                return null;
+
+            //remove [index] if property is array
+            if (path[path.Length -1] == ']')
+            {
+                int bracketIndex = path.LastIndexOf('[');
+                path = path.Remove(bracketIndex, path.LastIndexOf(']') - bracketIndex + 1);
+            }
+            //if parent is array, remove [index] also from it, so path will be parentName.property
+            if (path[i - 1] == ']')
+            {
+                int bracketIndex = path.LastIndexOf('[');
+                path = path.Remove(bracketIndex, path.LastIndexOf(']') - bracketIndex + 1);
+            }
+
+            //reset arrays in parents
+            path = path.Replace("[", ".Array.data[");
+        
+            //get the path to parent and return it
+            path = path.Substring(0, path.LastIndexOf('.'));
+            return property.serializedObject.FindProperty(path);
+        }
+
+        #endregion
+
+        #region property array
+
+        /// <summary>
+        /// Return property as array instead of elements inside of it
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        public static SerializedProperty GetPropertyArray(this SerializedProperty property)
+        {
+            //if this property is array, path is
+            //property.Array.data[index]
+
+            string path = property.propertyPath;
+
+            //remove Array.data[index] if property is array
+            if (path[path.Length - 1] == ']')
+            {
+                path = path.Remove(path.LastIndexOf('.'), 1);
+                path = path.Substring(0, path.LastIndexOf('.'));
+            }
+
+            //return it
+            return property.serializedObject.FindProperty(path);
+        }
+
+        /// <summary>
+        /// Return index of this element inside an array
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        public static int GetPropertyIndexInsideArray(this SerializedProperty property)
+        {
+            //if this property is array, path is
+            //property.Array.data[index]
+
+            string path = property.propertyPath;
+
+            //if array, remove brackets and read index
+            if (path[path.Length - 1] == ']')
+            {
+                int bracketIndex = path.LastIndexOf('[');
+                path = path.Replace("[", "").Replace("]", "");
+
+                if (int.TryParse(path.Substring(bracketIndex), out int result))
+                    return result;
+            }
+
+            return -1;
+        }
 
         #endregion
 
