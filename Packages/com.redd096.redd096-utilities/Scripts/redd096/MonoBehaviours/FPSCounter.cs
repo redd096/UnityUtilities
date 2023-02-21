e6ad2f9226dcf7991162ff5106771f6e07a0807f
redd096 <martin.babino96@gmail.com>
2023-02-21 18:24:05 +0100

    dynamic resolution

--- Packages/com.redd096.redd096-utilities/Scripts/redd096/MonoBehaviours/FPSCounter.cs
+++ Packages/com.redd096.redd096-utilities/Scripts/redd096/MonoBehaviours/FPSCounter.cs
@@ -5,15 +5,25 @@ namespace redd096
     [AddComponentMenu("redd096/MonoBehaviours/FPS Counter")]
     public class FPSCounter : MonoBehaviour
     {
-        [Header("Use GUI or UI Text - default UI get in children")]
-        [SerializeField] bool useOnGUI = true;
+        [Header("Show on Screen? Only editor or also in build?")]
+        [SerializeField] bool showOnScreen = true;
+        [SerializeField] bool showAlsoInBuild = false;
+
+        [Header("Use GUI or UI Text")]
+        [SerializeField] bool useOnGUI = false;
         [SerializeField] UnityEngine.UI.Text uiText = default;
+
+        [Header("DEBUG - Set FPS Limit")]
+        [SerializeField] bool setFpsLimit = false;
+        [SerializeField][Min(-1)] int limitFps = -1;
 
         private static FPSCounter instance;
         private int countFPS;
 
         private int lastFrameIndex;
         private float[] frameDeltaTimeArray;
+
+        public int CurrentFPS => countFPS;
 
         private void Awake()
         {
@@ -26,17 +36,47 @@ namespace redd096
             else
             {
                 Destroy(gameObject);
+                return;
             }
 
             //create array
             frameDeltaTimeArray = new float[50];
 
-            //get references
-            if (uiText == null)
-                uiText = GetComponentInChildren<UnityEngine.UI.Text>();
+            //hide canvas if can't show
+            if (CanShow() == false)
+            {
+                if (uiText != null)
+                    uiText.gameObject.SetActive(false);
+            }
+            //else show warning if not setted
+            else
+            {
+                if (CanShow() && useOnGUI == false && uiText == null)
+                    Debug.LogWarning($"Missing UIText on {this} - {gameObject}");
+            }
 
-            //warning
-            if (useOnGUI == false && uiText == null) Debug.LogWarning("Missing UIText on FPS Counter - " + gameObject);
+            //set fps limit if necessary
+            if (setFpsLimit)
+                Application.targetFrameRate = limitFps;
+        }
+
+        private void OnValidate()
+        {
+            //when change in inspector, be sure to update also canvas
+            if (CanShow() == false || useOnGUI)
+            {
+                if (uiText != null)
+                    uiText.gameObject.SetActive(false);
+            }
+            else if (CanShow() && useOnGUI == false)
+            {
+                if (uiText != null)
+                    uiText.gameObject.SetActive(true);
+            }
+
+            //set fps limit if necessary
+            if (setFpsLimit)
+                Application.targetFrameRate = limitFps;
         }
 
         private void Update()
@@ -56,8 +96,16 @@ namespace redd096
         private void OnGUI()
         {
             //if use OnGUI, set it
-            if (useOnGUI)
+            if (useOnGUI && CanShow())
                 GUI.Label(new Rect(30, 40, 100, 25), "FPS: " + countFPS);
+        }
+
+        #region private API
+
+        private bool CanShow()
+        {
+            //must be shown on screen, and is in editor or is enabled also in build
+            return showOnScreen && (Application.isEditor || showAlsoInBuild);
         }
 
         private float CalculateFPS()
@@ -71,5 +119,7 @@ namespace redd096
 
             return frameDeltaTimeArray.Length / total;
         }
+
+        #endregion
     }
 }
