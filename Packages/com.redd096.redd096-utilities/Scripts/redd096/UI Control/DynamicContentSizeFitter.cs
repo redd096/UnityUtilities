7be21208fb19a622c313b1c13fa4b2870bf4686f
redd096 <martin.babino96@gmail.com>
2023-06-09 11:48:21 +0200

    dynamic content size fitter with more rects

--- Packages/com.redd096.redd096-utilities/Scripts/redd096/UI Control/DynamicContentSizeFitter.cs
+++ Packages/com.redd096.redd096-utilities/Scripts/redd096/UI Control/DynamicContentSizeFitter.cs
@@ -49,14 +49,11 @@ namespace redd096
             Delta
         }
 
-        [Header("Content size based on rect to check")]
+        [Header("Content size based on rects to check")]
         [SerializeField] EDynamicFitMode m_dynamicFitMode = EDynamicFitMode.ExceedSize;
         [SerializeField] EDefaultSizeMode m_defaultSizeMode = EDefaultSizeMode.DeltaAndParentRect;
         [SerializeField] Vector2 m_sizeDelta = Vector2.one * 100;
-        [SerializeField] RectTransform m_rectToCheck = default;
-
-        [Header("Default rect to check size")]
-        [SerializeField] Vector2 m_rectToCheckDefaultSize = Vector2.one * 100;  //IncreaseWithRect - if rectToCheck exceed default size by 10, increase content size by 10
+        [SerializeField] RectToCheckStruct[] m_rectsToCheck = default;
 
         /// <summary>
         /// The dynamic fit mode to use to determine the size.
@@ -67,17 +64,13 @@ namespace redd096
         /// </summary>
         public EDefaultSizeMode defaultSizeMode { get { return m_defaultSizeMode; } set { m_defaultSizeMode = value; SetDirty(); } }
         /// <summary>
-        /// The normal size delta to use when we aren't using rect size
+        /// The normal size delta to use when we aren't using rects size
         /// </summary>
         public Vector2 sizeDelta { get { return m_sizeDelta; } set { m_sizeDelta = value; SetDirty(); } }
         /// <summary>
-        /// The rect we are checking to set the content size
+        /// The rects we are checking to set the content size
         /// </summary>
-        public RectTransform rectToCheck { get { return m_rectToCheck; } set { m_rectToCheck = value; SetDirty(); } }
-        /// <summary>
-        /// Default size of the rect to check
-        /// </summary>
-        public Vector2 rectToCheckDefaultSize { get { return m_rectToCheckDefaultSize; } set { m_rectToCheckDefaultSize = value; SetDirty(); } }
+        public RectToCheckStruct[] rectsToCheck { get { return m_rectsToCheck; } set { m_rectsToCheck = value; SetDirty(); } }
 
         //this object's rect transform (ContentSizeFitter copy-paste)
         [System.NonSerialized] private RectTransform m_Rect;
@@ -128,20 +121,21 @@ namespace redd096
 
             Vector2 previousSize = rectTransform.sizeDelta;
 
-            //float foundSize = (fitting == FitMode.MinSize) ? 
-            //    DynamicSize(LayoutUtility.GetMinSize(m_rectToCheck, axis), axis) : 
-            //    DynamicSize(LayoutUtility.GetPreferredSize(m_rectToCheck, axis), axis);
-            //
-            //Debug.Log($"<color=green>BEFORE - delta: {sizeDelta} - parent rect width: {parentRectTransform.rect.width} - otherRect width: {rectToCheck.rect.width} - " +
-            //    $"current rect width: {rectTransform.rect.width} -  found size: {foundSize} - current sizeDelta: {rectTransform.sizeDelta}</color>");
-            //
-            //rectTransform.SetSizeWithCurrentAnchors((RectTransform.Axis)axis, foundSize);
+            //sum every rect size
+            float rectsSize = 0;
+            Vector2 defaultSize = Vector2.zero;
+            foreach (RectToCheckStruct rectStruct in m_rectsToCheck)
+            {
+                if (fitting == FitMode.MinSize)
+                    rectsSize += LayoutUtility.GetMinSize(rectStruct.rect, axis);
+                else
+                    rectsSize += LayoutUtility.GetPreferredSize(rectStruct.rect, axis);
+
+                defaultSize += rectStruct.defaultSize;
+            }
 
             // Set size to min or preferred size
-            if (fitting == FitMode.MinSize)
-                rectTransform.SetSizeWithCurrentAnchors((RectTransform.Axis)axis, DynamicSize(LayoutUtility.GetMinSize(m_rectToCheck, axis), axis));
-            else
-                rectTransform.SetSizeWithCurrentAnchors((RectTransform.Axis)axis, DynamicSize(LayoutUtility.GetPreferredSize(m_rectToCheck, axis), axis));
+            rectTransform.SetSizeWithCurrentAnchors((RectTransform.Axis)axis, DynamicSize(rectsSize, axis, defaultSize));
 
             //Debug.Log($"<color=green>AFTER - delta: {sizeDelta} - parent rect width: {parentRectTransform.rect.width} - otherRect width: {rectToCheck.rect.width} - " +
             //    $"current rect width: {rectTransform.rect.width} -  found size: {foundSize} - current sizeDelta: {rectTransform.sizeDelta}</color>");
@@ -152,7 +146,7 @@ namespace redd096
 #endif
         }
 
-        float DynamicSize(float size, int axis)
+        float DynamicSize(float size, int axis, Vector2 defaultSize)
         {
             //if follow always rect, return its size
             if (dynamicFitMode == EDynamicFitMode.FollowAlwaysRect)
@@ -178,7 +172,7 @@ namespace redd096
             //check if need to increase with rect
             if (dynamicFitMode == EDynamicFitMode.IncreaseWithRect)
             {
-                float rectDefaultSize = axis == 0 ? m_rectToCheckDefaultSize.x : m_rectToCheckDefaultSize.y;
+                float rectDefaultSize = axis == 0 ? defaultSize.x : defaultSize.y;
                 //Debug.Log($"axis {axis} - contentSize {contentSize} - defaultRect {rectDefaultSize} - currentRect {size} - result {contentSize + size - rectDefaultSize}");
                 if (size > rectDefaultSize)
                 {
@@ -188,6 +182,19 @@ namespace redd096
             }
 
             return contentSize;
+        }
+
+        [System.Serializable]
+        public struct RectToCheckStruct
+        {
+            /// <summary>
+            /// The rect we are checking to set the content size
+            /// </summary>
+            public RectTransform rect;
+            /// <summary>
+            /// Used in some dynamic fit mode. For example IncreaseWithRect -> if rectToCheck exceed default size by 10, increase content size by 10
+            /// </summary>
+            public Vector2 defaultSize;
         }
     }
 
@@ -201,8 +208,7 @@ namespace redd096
         SerializedProperty m_dynamicFitMode;
         SerializedProperty m_defaultSizeMode;
         SerializedProperty m_sizeDelta;
-        SerializedProperty m_rectToCheck;
-        SerializedProperty m_rectToCheckDefaultSize;
+        SerializedProperty m_rectsToCheck;
 
         protected override void OnEnable()
         {
@@ -210,8 +216,7 @@ namespace redd096
             m_dynamicFitMode = serializedObject.FindProperty("m_dynamicFitMode");
             m_defaultSizeMode = serializedObject.FindProperty("m_defaultSizeMode");
             m_sizeDelta = serializedObject.FindProperty("m_sizeDelta");
-            m_rectToCheck = serializedObject.FindProperty("m_rectToCheck");
-            m_rectToCheckDefaultSize = serializedObject.FindProperty("m_rectToCheckDefaultSize");
+            m_rectsToCheck = serializedObject.FindProperty("m_rectsToCheck");
         }
 
         public override void OnInspectorGUI()
@@ -230,11 +235,7 @@ namespace redd096
             EditorGUILayout.PropertyField(m_dynamicFitMode, true);
             EditorGUILayout.PropertyField(m_defaultSizeMode, true);
             EditorGUILayout.PropertyField(m_sizeDelta, true);
-            EditorGUILayout.PropertyField(m_rectToCheck, true);
-
-            //show this only with specifics dynamic fit mode
-            if ((DynamicContentSizeFitter.EDynamicFitMode)m_dynamicFitMode.enumValueIndex == DynamicContentSizeFitter.EDynamicFitMode.IncreaseWithRect)
-                EditorGUILayout.PropertyField(m_rectToCheckDefaultSize, true);
+            EditorGUILayout.PropertyField(m_rectsToCheck, true);
 
             serializedObject.ApplyModifiedProperties();
         }
