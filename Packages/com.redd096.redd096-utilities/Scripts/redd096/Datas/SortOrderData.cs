3d8a534f78972b73f7f02d81b6b3706918fccfc0
redd096 <martin.babino96@gmail.com>
2024-02-20 19:05:10 +0100

    update

--- Packages/com.redd096.redd096-utilities/Scripts/redd096/Datas/SortOrderData.cs
+++ Packages/com.redd096.redd096-utilities/Scripts/redd096/Datas/SortOrderData.cs
@@ -2,51 +2,54 @@ using redd096.Attributes;
 using System.Reflection;
 using UnityEngine;
 
-/// <summary>
-/// Scriptable object used to set SortingLayer and OrderInLayer or SortOrder, in various Renderer and Canvas
-/// </summary>
-[CreateAssetMenu(fileName = "SortOrderData", menuName = "redd096/Sort Order Data")]
-public class SortOrderData : ScriptableObject
+namespace redd096
 {
-    public Element[] Elements;
-
-    public Element GetElement(string elementName)
+    /// <summary>
+    /// Scriptable object used to set SortingLayer and OrderInLayer or SortOrder, in various Renderer and Canvas
+    /// </summary>
+    [CreateAssetMenu(fileName = "SortOrderData", menuName = "redd096/Sort Order Data")]
+    public class SortOrderData : ScriptableObject
     {
-        //find element in array by name
-        if (Elements != null)
+        public Element[] Elements;
+
+        public Element GetElement(string elementName)
         {
-            foreach (var element in Elements)
-                if (element.Name == elementName)
-                    return element;
+            //find element in array by name
+            if (Elements != null)
+            {
+                foreach (var element in Elements)
+                    if (element.Name == elementName)
+                        return element;
+            }
+
+            Debug.LogError("Impossible to find: " + elementName);
+            return null;
         }
 
-        Debug.LogError("Impossible to find: " + elementName);
-        return null;
-    }
-
-    [System.Serializable]
-    public class Element
-    {
-        public string Name;
-        [Tooltip("Used both on Renderer (SpriteRenderer, MeshRenderer...) and Canvas world")][Dropdown("GetSortingLayers")] public int SortingLayer;
-        [Tooltip("Sort order in Canvas overlay, OrderInLayer in Renderer (SpriteRenderer, MeshRenderer...) and Canvas world")] public int OrderInLayer;
+        [System.Serializable]
+        public class Element
+        {
+            public string Name;
+            [Tooltip("Used both on Renderer (SpriteRenderer, MeshRenderer...) and Canvas world")][Dropdown("GetSortingLayers")] public int SortingLayer;
+            [Tooltip("Sort order in Canvas overlay, OrderInLayer in Renderer (SpriteRenderer, MeshRenderer...) and Canvas world")] public int OrderInLayer;
 
 #if UNITY_EDITOR
-        DropdownList<int> GetSortingLayers()
-        {
-            //get sorting layer IDs
-            System.Type internalEditorUtilityType = typeof(UnityEditorInternal.InternalEditorUtility);
-            PropertyInfo sortingLayerUniqueIDsProperty = internalEditorUtilityType.GetProperty("sortingLayerUniqueIDs", BindingFlags.Static | BindingFlags.NonPublic);
-            int[] sortIDs = (int[])sortingLayerUniqueIDsProperty.GetValue(null, new object[0]);
-
-            //set dropdown
-            DropdownList<int> list = new DropdownList<int>();
-            for (int i = 0; i < sortIDs.Length; i++)
+            DropdownList<int> GetSortingLayers()
             {
-                list.Add(UnityEngine.SortingLayer.IDToName(sortIDs[i]), sortIDs[i]);
+                //get sorting layer IDs
+                System.Type internalEditorUtilityType = typeof(UnityEditorInternal.InternalEditorUtility);
+                PropertyInfo sortingLayerUniqueIDsProperty = internalEditorUtilityType.GetProperty("sortingLayerUniqueIDs", BindingFlags.Static | BindingFlags.NonPublic);
+                int[] sortIDs = (int[])sortingLayerUniqueIDsProperty.GetValue(null, new object[0]);
+
+                //set dropdown
+                DropdownList<int> list = new DropdownList<int>();
+                for (int i = 0; i < sortIDs.Length; i++)
+                {
+                    list.Add(UnityEngine.SortingLayer.IDToName(sortIDs[i]), sortIDs[i]);
+                }
+                return list;
             }
-            return list;
-        }
 #endif
+        }
     }
 }
--- Packages/com.redd096.redd096-utilities/Scripts/redd096/MonoBehaviours/SortOrderController.cs
+++ Packages/com.redd096.redd096-utilities/Scripts/redd096/MonoBehaviours/SortOrderController.cs
@@ -1,72 +1,77 @@
 using redd096.Attributes;
 using UnityEngine;
 
-/// <summary>
-/// Used to manage SortingLayer and OrderInLayer
-/// </summary>
-public class SortOrderController : MonoBehaviour
+namespace redd096
 {
-    //inspector
-    [SerializeField] SortOrderData data;
-    [Dropdown("GetNames")][SerializeField] string elementName;
-
-    [Header("Canvas or Renderer - if both null, try get component in children")]
-    [SerializeField] bool updateOnAwake = true;
-    [SerializeField] Canvas canvas;
-    [SerializeField] Renderer rend;
-
-    //get from data
-    private SortOrderData.Element element;
-    private SortOrderData.Element GetElement() { if (data == null) Debug.LogError("Missing Data!"); return data ? data.GetElement(elementName) : null; }
-
-    private void Awake()
+    /// <summary>
+    /// Used to manage SortingLayer and OrderInLayer
+    /// </summary>
+    [AddComponentMenu("redd096/MonoBehaviours/Sort Order Controller")]
+    public class SortOrderController : MonoBehaviour
     {
-        if (updateOnAwake)
-            UpdateSortOrder();
-    }
+        //inspector
+        [SerializeField] SortOrderData data;
+        [Dropdown("GetNames")][SerializeField] string elementName;
 
-    [Button]
-    void UpdateSortOrder()
-    {
-        //get ref
-        if (canvas == null && rend == null)
+        [Header("Canvas or Renderer - if both null, try get component in children")]
+        [SerializeField] bool updateOnAwake = true;
+        [SerializeField] Canvas canvas;
+        [SerializeField] Renderer rend;
+
+        //get from data
+        private SortOrderData.Element _element;
+        private SortOrderData.Element GetElement() { if (data == null) Debug.LogError("Missing Data!"); return data ? data.GetElement(elementName) : null; }
+        public SortOrderData.Element Element { get { if (_element == null || string.IsNullOrEmpty(_element.Name)) _element = GetElement(); return _element; } }
+
+        private void Awake()
         {
-            canvas = GetComponentInChildren<Canvas>();
-            if (canvas == null) rend = GetComponentInChildren<Renderer>();
+            if (updateOnAwake)
+                UpdateSortOrder();
         }
 
-        //update element
-        element = GetElement();
-        if (element == null)
+        [Button]
+        void UpdateSortOrder()
         {
-            return;
-        }
+            //get ref
+            if (canvas == null && rend == null)
+            {
+                canvas = GetComponentInChildren<Canvas>();
+                if (canvas == null) rend = GetComponentInChildren<Renderer>();
+            }
 
-        //update sort order
-        if (canvas != null)
-        {
-            canvas.sortingLayerID = element.SortingLayer;
-            canvas.sortingOrder = element.OrderInLayer;
-        }
-        if (rend != null)
-        {
-            rend.sortingLayerID = element.SortingLayer;
-            rend.sortingOrder = element.OrderInLayer;
-        }
+            //refresh element
+            _element = GetElement();
+            if (Element == null)
+            {
+                return;
+            }
 
-    }
+            //update sort order
+            if (canvas != null)
+            {
+                canvas.sortingLayerID = _element.SortingLayer;
+                canvas.sortingOrder = _element.OrderInLayer;
+            }
+            if (rend != null)
+            {
+                rend.sortingLayerID = _element.SortingLayer;
+                rend.sortingOrder = _element.OrderInLayer;
+            }
+
+        }
 
 #if UNITY_EDITOR
-    string[] GetNames()
-    {
-        if (data == null)
-            return new string[0];
+        string[] GetNames()
+        {
+            if (data == null)
+                return new string[0];
 
-        string[] s = new string[data.Elements.Length];
-        for (int i = 0; i < s.Length; i++)
-            s[i] = data.Elements[i].Name;
+            string[] s = new string[data.Elements.Length];
+            for (int i = 0; i < s.Length; i++)
+                s[i] = data.Elements[i].Name;
 
-        return s;
-    }
+            return s;
+        }
 #endif
+    }
 }
