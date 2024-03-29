0f6771f6f40ced81735d879b6875cae32b667a83
redd096 <martin.babino96@gmail.com>
2024-03-29 16:21:28 +0100

    Force Refresh UI component

---
+++ Packages/com.redd096.redd096-utilities/Scripts/redd096/UI Control/ForceRefreshUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace redd096
+{
+    [AddComponentMenu("redd096/UI Control/Force Refresh UI")]
+    public class ForceRefreshUI : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            StartCoroutine(RefreshLayoutGroupsImmediateAndRecursive(gameObject));
+        }
+
+        /// <summary>
+        /// Wait end of frame and call ForceRebuildLayoutImmediate on every child layout group
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static IEnumerator RefreshLayoutGroupsImmediateAndRecursive(GameObject root)
+        {
+            //wait end of frame
+            yield return new WaitForEndOfFrame();
+
+            //refresh every child layout group
+            var componentsInChildren = root.GetComponentsInChildren<LayoutGroup>(true);
+            foreach (var layoutGroup in componentsInChildren)
+            {
+                LayoutRebuilder.ForceRebuildLayoutImmediate(layoutGroup.GetComponent<RectTransform>());
+            }
+
+            //be sure root is refreshed as last one
+            var rootRect = root.GetComponent<RectTransform>();
+            if (rootRect)
+                LayoutRebuilder.ForceRebuildLayoutImmediate(rootRect);
+        }
+    }
+}
