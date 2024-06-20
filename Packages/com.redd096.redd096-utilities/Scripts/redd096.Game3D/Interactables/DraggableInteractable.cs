517a24587da4eb53fe2adbb30fff59217b3cf728
redd096 <martin.babino96@gmail.com>
2024-06-20 17:02:50 +0200

    DraggableJoint Interactable

--- Packages/com.redd096.redd096-utilities/Scripts/redd096.Game3D/Interactables/DraggableInteractable.cs
+++ Packages/com.redd096.redd096-utilities/Scripts/redd096.Game3D/Interactables/DraggableInteractable.cs
@@ -12,11 +12,15 @@ namespace redd096.Game3D
     {
         [Header("Necessary Components (by default get from this gameObject)")]
         [SerializeField] Rigidbody rb;
+        [Tooltip("When stop drag: press to toggle drag or keep pressed?")][SerializeField] EDismissType dismissType = EDismissType.InteractAgain;
         [Tooltip("When dragged, save offset on interact or use fixed offset setted in inspector")][SerializeField] bool useFixedOffset = true;
         [EnableIf("useFixedOffset")][Tooltip("When dragged, this is the offset to keep from interactor")][SerializeField] Vector3 offsetFromInteractor = Vector3.forward * 2;
         [SerializeField] EDraggableConstraints draggableConstraints = EDraggableConstraints.None;
         [EnableIf("draggableConstraints", EDraggableConstraints.EditRigidbodyConstraintsOnDrag)][EnumFlags][SerializeField] RigidbodyConstraints constraintsOnDrag;
         [SerializeField] float lerpMovement = 10f;
+
+        //call OnDismiss when release interact button, press again interact or other
+        public EDismissType DismissType => dismissType;
 
         //events
         public System.Action onInteract;
@@ -54,6 +58,7 @@ namespace redd096.Game3D
                 Vector3 grabOffsetRotated = camTransform.position + camTransform.TransformDirection(offset);
                 rb.MovePosition(Vector3.Lerp(rb.position, grabOffsetRotated, Time.fixedDeltaTime * lerpMovement));
 
+                //rotate if necessary
                 if (draggableConstraints == EDraggableConstraints.AlwaysLookInUserDirection && interactor)
                 {
                     rb.MoveRotation(interactor.transform.rotation);
@@ -98,16 +103,16 @@ namespace redd096.Game3D
 
             return true;
         }
-    }
 
-    /// <summary>
-    /// Used to decide how this object will move and rotate when dragged
-    /// None - normally, just stop gravity
-    /// EditRigidbodyConstraintsOnDrag - set new rigidbody constraints. Reset on release
-    /// AlwaysLookInUserDirection - while dragged, always rotate to look in the same direction as interactor
-    /// </summary>
-    public enum EDraggableConstraints
-    {
-        None, EditRigidbodyConstraintsOnDrag, AlwaysLookInUserDirection
+        /// <summary>
+        /// Used to decide how this object will move and rotate when dragged
+        /// None - normally, just stop gravity
+        /// EditRigidbodyConstraintsOnDrag - set new rigidbody constraints. Reset on release
+        /// AlwaysLookInUserDirection - while dragged, always rotate to look in the same direction as interactor
+        /// </summary>
+        public enum EDraggableConstraints
+        {
+            None, EditRigidbodyConstraintsOnDrag, AlwaysLookInUserDirection
+        }
     }
 }
---
+++ Packages/com.redd096.redd096-utilities/Scripts/redd096.Game3D/Interactables/DraggableJointInteractable.cs
@@ -0,0 +1,248 @@
+using redd096.Attributes;
+using System.Collections;
+using UnityEngine;
+
+namespace redd096.Game3D
+{
+    /// <summary>
+    /// Script to interact and start drag objects in scene, by using a Joint
+    /// </summary>
+    [AddComponentMenu("redd096/.Game3D/Interactables/Draggable Joint Interactable")]
+    public class DraggableJointInteractable : MonoBehaviour
+    {
+        [Header("Necessary Components (by default get from this gameObject)")]
+        [SerializeField] Rigidbody rb;
+        [Tooltip("When stop drag: press to toggle drag or keep pressed?")][SerializeField] EDismissType dismissType = EDismissType.ReleaseInteractInput;
+        [Tooltip("If user moves too much away from the dragged object, destroy the joint")][SerializeField] float breakJointRange = 2;
+        [Tooltip("If using something like doors, set to false. If dragging a pen that follows player, set to true")][SerializeField] bool canDragAround = false;
+        [SerializeField] EDraggableConstraints draggableConstraints = EDraggableConstraints.None;
+        [EnableIf("draggableConstraints", EDraggableConstraints.EditRigidbodyConstraintsOnDrag)][EnumFlags][SerializeField] RigidbodyConstraints constraintsOnDrag;
+        [Tooltip("If true, when drag set Interpolate and ContinuousDynamic. Reset when stop drag")][SerializeField] bool setInterpolationAndCollisionDetectionMode = true;
+
+        //call OnDismiss when release interact button, press again interact or other
+        public EDismissType DismissType => dismissType;
+
+        //events
+        public System.Action onInteract;
+        public System.Action onDismiss;
+        public System.Action onTooMuchDistance;
+
+        public Rigidbody Rb => rb;
+
+        InteractComponent interactor;
+        Transform camTransform;
+
+        //for grab - instantiate and move an hand with a joint to the grabbed object
+        Transform handle;   //used to check the distance to break the joint
+        Rigidbody handRb;
+
+        //previous rigidbody values
+        bool previousUseGravity;
+        RigidbodyConstraints previousConstraints;
+        RigidbodyInterpolation previousInterpolation;
+        CollisionDetectionMode previousCollisionDetectionMode;
+
+        protected virtual void Awake()
+        {
+            //be sure to have a components
+            if (rb == null && TryGetComponent(out rb) == false)
+                Debug.LogError("Miss Rigidbody on " + name);
+        }
+
+        protected virtual void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow - new Color(0, 0, 0, 0.7f);
+            if (handle)
+                Gizmos.DrawSphere(handle.position, 0.1f);
+            Gizmos.color = Color.red - new Color(0, 0, 0, 0.7f);
+            if (handRb)
+                Gizmos.DrawSphere(handRb.position, 0.1f);
+            Gizmos.color = Color.white;
+        }
+
+        protected virtual void OnEnable()
+        {
+            //when disabled, coroutine are stopped. So restart movement coroutine if necessary
+            if (handRb)
+                StartCoroutine(MovementCoroutine());
+        }
+
+        protected virtual IEnumerator MovementCoroutine()
+        {
+            while (handRb != null && handle != null)
+            {
+                //if too much distance, destroy joint
+                if (Vector3.Distance(handRb.transform.position, handle.transform.position) > breakJointRange)
+                {
+                    DestroyJoint(false);
+                    onTooMuchDistance?.Invoke();
+                    interactor.DismissWithCurrentInteractable();
+                    break;
+                }
+
+                //move hand
+                //bool hitSomething = Physics.Raycast(camTransform.position, camTransform.forward, out RaycastHit hit, breakJointRange);
+                //handRb.transform.position = hitSomething ? hit.point : camTransform.position + camTransform.forward * breakJointRange;
+
+                //rotate if necessary
+                if (draggableConstraints == EDraggableConstraints.AlwaysLookInUserDirection)
+                {
+                    rb.MoveRotation(interactor.transform.rotation);
+                }
+
+                yield return new WaitForFixedUpdate();
+            }
+        }
+
+        public virtual bool OnInteract(InteractComponent interactor, RaycastHit hit, params object[] args)
+        {
+            this.interactor = interactor;
+            camTransform = args[0] as Transform;
+
+            //save previous rigidbody values
+            previousUseGravity = rb.useGravity;
+            previousConstraints = rb.constraints;
+            previousInterpolation = rb.interpolation;
+            previousCollisionDetectionMode = rb.collisionDetectionMode;
+
+            //edit rigidbody values
+            rb.useGravity = false;
+            if (draggableConstraints == EDraggableConstraints.EditRigidbodyConstraintsOnDrag) rb.constraints = constraintsOnDrag;
+            if (setInterpolationAndCollisionDetectionMode) rb.interpolation = RigidbodyInterpolation.Interpolate;
+            if (setInterpolationAndCollisionDetectionMode) rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+
+            //create joint
+            CreateJoint(hit.point);
+
+            //start coroutine
+            StartCoroutine(MovementCoroutine());
+
+            onInteract?.Invoke();
+
+            return true;
+        }
+
+        public virtual bool OnDismiss(InteractComponent interactor, params object[] args)
+        {
+            this.interactor = null;
+            camTransform = null;
+
+            //be sure to stop coroutine
+            StopCoroutine(nameof(MovementCoroutine));
+
+            //reset rigidbody
+            rb.useGravity = previousUseGravity;
+            if (draggableConstraints == EDraggableConstraints.EditRigidbodyConstraintsOnDrag) rb.constraints = previousConstraints;
+            if (setInterpolationAndCollisionDetectionMode) rb.interpolation = previousInterpolation;
+            if (setInterpolationAndCollisionDetectionMode) rb.collisionDetectionMode = previousCollisionDetectionMode;
+
+            //destroy joint
+            DestroyJoint(false);
+
+            onDismiss?.Invoke();
+
+            return true;
+        }
+
+        protected virtual void CreateJoint(Vector3 hitPoint)
+        {
+            //remove if has already joint
+            DestroyJoint(false);
+
+            //create handle, child of the object to drag
+            handle = new GameObject("Handle").transform;
+            handle.transform.SetParent(rb.transform);
+            handle.position = hitPoint;
+
+            //create hand
+            handRb = new GameObject("Hand", typeof(Rigidbody)).GetComponent<Rigidbody>();
+            handRb.isKinematic = true;
+            handRb.transform.SetParent(camTransform);
+            handRb.transform.position = handle.position;
+
+            //create joint - old
+            //SpringJoint joint = handRb.gameObject.AddComponent<SpringJoint>();
+            //joint.spring = 50;
+            //joint.damper = 5;
+            //joint.maxDistance = 0.2f;
+            //joint.connectedBody = rb;
+            //joint.anchor = Vector3.zero;
+
+            //create joint
+            ConfigurableJoint joint = handRb.gameObject.AddComponent<ConfigurableJoint>();
+            JointDrive drive = new()
+            {
+                positionSpring = 80f,
+                positionDamper = 32f,
+                maximumForce = Mathf.Infinity
+            };
+            joint.xDrive = drive;
+            joint.yDrive = drive;
+            joint.zDrive = drive;
+            joint.connectedBody = rb;
+            joint.anchor = Vector3.zero;
+
+            //configure joint to take things that follow player
+            if (canDragAround)
+            {
+                JointDrive angularDrive = new()
+                {
+                    positionSpring = 100f,
+                    positionDamper = 40f,
+                    maximumForce = Mathf.Infinity
+                };
+                joint.angularXDrive = angularDrive;
+                joint.angularYZDrive = angularDrive;
+                joint.xMotion = ConfigurableJointMotion.Limited;
+                joint.yMotion = ConfigurableJointMotion.Limited;
+                joint.zMotion = ConfigurableJointMotion.Limited;
+                SoftJointLimit limit = new()
+                {
+                    bounciness = 0,
+                    limit = 0.15f
+                };
+                joint.linearLimit = limit;
+            }
+        }
+
+        protected virtual void DestroyJoint(bool throwed)
+        {
+            if (handRb == null)
+                return;
+
+            //calculate throw speed
+            Vector3 delta = Vector3.zero;
+            //if (rb.Inertia)
+            delta = handRb.position - handle.position;
+
+            if (throwed)
+                throwed = delta.magnitude > 0.2f;
+
+            delta = Vector3.ClampMagnitude(delta, 0.5f);
+
+            //destroy hand and handle
+            Destroy(handle.gameObject);
+            Destroy(handRb.gameObject);
+            handle = null;
+            handRb = null;
+
+            //throw rigidbody
+            if (throwed)
+            {
+                Vector3 force = delta * 20;
+                rb.AddForce(force, ForceMode.Impulse);
+            }
+        }
+
+        /// <summary>
+        /// Used to decide how this object will move and rotate when dragged
+        /// None - normally, just stop gravity
+        /// EditRigidbodyConstraintsOnDrag - set new rigidbody constraints. Reset on release
+        /// AlwaysLookInUserDirection - while dragged, always rotate to look in the same direction as interactor
+        /// </summary>
+        public enum EDraggableConstraints
+        {
+            None, EditRigidbodyConstraintsOnDrag, AlwaysLookInUserDirection
+        }
+    }
+}
