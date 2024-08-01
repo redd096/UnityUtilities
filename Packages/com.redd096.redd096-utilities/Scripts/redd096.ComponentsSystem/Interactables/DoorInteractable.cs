01e7c4ffeb37dcf2e4b4dc4e1cd7cad112302b7a
redd096 <martin.babino96@gmail.com>
2024-08-01 22:45:38 +0200

    interactables in ComponentsSystem

---
+++ Packages/com.redd096.redd096-utilities/Scripts/redd096.ComponentsSystem/Interactables/DoorInteractable.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using UnityEngine;
+
+namespace redd096.ComponentsSystem
+{
+    /// <summary>
+    /// On interact, open door
+    /// </summary>
+    [AddComponentMenu("redd096/.ComponentsSystem/Interactables/Door Interactable")]
+    public class DoorInteractable : MonoBehaviour, IInteractable
+    {
+        [SerializeField] Transform objectToMove;
+        [SerializeField] float movementSpeed = 2;
+        [SerializeField] float closeAfterSeconds = 3f;
+        [SerializeField] Vector2 openPosition;
+
+        bool isOpen;
+
+        public void Interact(ICharacter interactor)
+        {
+            //do nothing if already open
+            if (isOpen)
+                return;
+
+            StartCoroutine(MovementAnimation());
+        }
+
+        private IEnumerator MovementAnimation()
+        {
+            //calculate duration open animation
+            Vector2 startPosition = objectToMove.localPosition;
+            float distance = Vector2.Distance(startPosition, openPosition);
+            float timeDuration = distance / movementSpeed;
+
+            //open animation
+            float delta = 0;
+            while (delta < 1)
+            {
+                delta += Time.deltaTime / timeDuration;
+                objectToMove.localPosition = Vector2.Lerp(startPosition, openPosition, delta);
+                yield return null;
+            }
+
+            isOpen = true;
+
+            //wait
+            yield return new WaitForSeconds(closeAfterSeconds);
+
+            //close animation
+            delta = 0;
+            while (delta < 1)
+            {
+                delta += Time.deltaTime / timeDuration;
+                objectToMove.localPosition = Vector2.Lerp(openPosition, startPosition, delta);
+                yield return null;
+            }
+
+            isOpen = false;
+        }
+    }
+}
