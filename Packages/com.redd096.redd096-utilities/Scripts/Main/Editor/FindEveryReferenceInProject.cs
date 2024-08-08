ad33660510fc8940d236cf3123432b6be8313785
redd096 <martin.babino96@gmail.com>
2024-08-08 18:26:24 +0200

    progress bar for FindEveryReferenceInProject

--- Packages/com.redd096.redd096-utilities/Scripts/Main/Editor/FindEveryReferenceInProject.cs
+++ Packages/com.redd096.redd096-utilities/Scripts/Main/Editor/FindEveryReferenceInProject.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.Threading.Tasks;
 
 namespace redd096
 {
@@ -10,11 +11,12 @@ namespace redd096
         private const string MenuItemText = "Assets/Find EVERY Reference In Project";
 
         [MenuItem(MenuItemText, false, 25)]
-        public static void Find()
+        public static async void Find()
         {
             var sw = new System.Diagnostics.Stopwatch();
             sw.Start();
 
+            //save every asset and its dependencies in the project
             var referenceCache = new Dictionary<string, List<string>>();
 
             string[] guids = AssetDatabase.FindAssets("");
@@ -23,31 +25,45 @@ namespace redd096
                 string assetPath = AssetDatabase.GUIDToAssetPath(guid);
                 string[] dependencies = AssetDatabase.GetDependencies(assetPath, false);
 
-                foreach (var dependency in dependencies)
+                for (int i = 0; i < dependencies.Length; i++)
                 {
-                    if (referenceCache.ContainsKey(dependency))
+                    if (referenceCache.ContainsKey(dependencies[i]))
                     {
-                        if (!referenceCache[dependency].Contains(assetPath))
+                        if (!referenceCache[dependencies[i]].Contains(assetPath))
                         {
-                            referenceCache[dependency].Add(assetPath);
+                            referenceCache[dependencies[i]].Add(assetPath);
                         }
                     }
                     else
                     {
-                        referenceCache[dependency] = new List<string>() { assetPath };
+                        referenceCache[dependencies[i]] = new List<string>() { assetPath };
                     }
+
+                    //show progress bar and delay of one frame
+                    EditorUtility.DisplayProgressBar($"Saving every dependency for asset at path: {assetPath}", $"Saving... {i}/{dependencies.Length}", (float)i / dependencies.Length);
+                    if (i % 20 == 0)
+                        await Task.Delay((int)(Time.deltaTime * 1000));
                 }
             }
 
-            Debug.Log("Build index takes " + sw.ElapsedMilliseconds + " milliseconds");
+            Debug.Log($"Build index takes {sw.ElapsedMilliseconds} milliseconds");
 
+            //find every reference for the selected object
             string path = AssetDatabase.GetAssetPath(Selection.activeObject);
-            Debug.Log("Find: " + path, Selection.activeObject);
+            Debug.Log($"Find references for asset at path: {path}", Selection.activeObject);
+
             if (referenceCache.ContainsKey(path))
             {
-                foreach (var reference in referenceCache[path])
+                List<string> references = referenceCache[path];
+                for (int i = 0; i < references.Count; i++)
                 {
-                    Debug.Log(reference, AssetDatabase.LoadMainAssetAtPath(reference));
+                    //stamp reference file
+                    Debug.Log(references[i], AssetDatabase.LoadMainAssetAtPath(references[i]));
+
+                    //show progress bar and delay of one frame
+                    EditorUtility.DisplayProgressBar($"Finding every reference", $"Finding... {i}/{references.Count}", (float)i / references.Count);
+                    if (i % 20 == 0)
+                        await Task.Delay((int)(Time.deltaTime * 1000));
                 }
             }
             else
