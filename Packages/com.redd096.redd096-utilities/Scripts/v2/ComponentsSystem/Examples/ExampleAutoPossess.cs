803044f277cacab01ecc8fa8c6d5d22c53784c53
redd096 <martin.babino96@gmail.com>
2024-08-13 19:07:10 +0200

    update example auto possess

--- Packages/com.redd096.redd096-utilities/Scripts/v2/ComponentsSystem/Examples/ExampleAutoPossess.cs
+++ Packages/com.redd096.redd096-utilities/Scripts/v2/ComponentsSystem/Examples/ExampleAutoPossess.cs
@@ -3,23 +3,34 @@ using UnityEngine;
 namespace redd096.v2.ComponentsSystem.Example
 {
     /// <summary>
-    /// Just tell to PlayerController to possess Pawn on awake
+    /// Just tell to every PlayerController in scene to possess a Pawn on awake
     /// </summary>
     [AddComponentMenu("redd096/v2/ComponentsSystem/Examples/Example Auto Possess")]
     public class ExampleAutoPossess : MonoBehaviour
     {
         private void Awake()
         {
-            //get pawns in scene
-            PlayerController playerController = GetComponent<PlayerController>();
-            PlayerPawn[] pawns = FindObjectsOfType<PlayerPawn>();
+            //get player controllers and pawns in scene
+            PlayerController[] playerControllers = FindObjectsByType<PlayerController>(FindObjectsInactive.Include, FindObjectsSortMode.InstanceID);
+            PlayerPawn[] pawns = FindObjectsByType<PlayerPawn>(FindObjectsInactive.Include, FindObjectsSortMode.InstanceID);
 
-            //find first pawn without player controller 
-            for (int i = 0; i < pawns.Length; i++)
+            //foreach player controller
+            foreach (var playerController in playerControllers)
             {
-                if (pawns[i].CurrentController == null)
+                //find first pawn without player controller and possess
+                for (int i = 0; i < pawns.Length; i++)
                 {
-                    pawns[i].Possess(playerController);
+                    if (pawns[i].CurrentController == null)
+                    {
+                        pawns[i].Possess(playerController);
+                        //pawns[i].GetComponent<ICharacter>().GetCharacterComponent<InspectorStateMachineComponent>().ResetToFirstState();
+                        break;
+                    }
+                }
+
+                if (playerController.CurrentPawn == null)
+                {
+                    Debug.LogError($"No more pawns to possess. {playerController.name} is still without a pawn", gameObject);
                 }
             }
         }
