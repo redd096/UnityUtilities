d10a27e52eb3b7e277bd7247bc95e772d227cd8d
redd096 <martin.babino96@gmail.com>
2024-09-13 23:03:49 +0200

    update LimitsManager

--- Packages/com.redd096.redd096-utilities/Scripts/Main/MonoBehaviours/LimitsManager.cs
+++ Packages/com.redd096.redd096-utilities/Scripts/Main/MonoBehaviours/LimitsManager.cs
@@ -1,171 +1,133 @@
-﻿using System.Collections.Generic;
+﻿using redd096.Attributes;
 using UnityEngine;
 
 namespace redd096
 {
-    #region editor
-#if UNITY_EDITOR
-
-    using UnityEditor;
-
-    [CustomEditor(typeof(LimitsManager))]
-    public class LimitsManagerEditor : Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            base.OnInspectorGUI();
-
-            GUILayout.Space(10);
-
-            //button regen
-            if (GUILayout.Button("Regen Limits"))
-            {
-                //set undo
-                Undo.RegisterFullObjectHierarchyUndo(target, "Regen Limits");
-
-                ((LimitsManager)target).RegenLimits();
-
-                //repaint scene
-                SceneView.RepaintAll();
-            }
-
-            //button destroy
-            if (GUILayout.Button("Destroy Limits"))
-            {
-                //set undo
-                Undo.RegisterFullObjectHierarchyUndo(target, "Regen Limits");
-
-                ((LimitsManager)target).DestroyLimits();
-
-                //repaint scene
-                SceneView.RepaintAll();
-            }
-        }
-    }
-
-#endif
-    #endregion
-
+    /// <summary>
+    /// Generate Colliders at the limits of the camera
+    /// </summary>
     [AddComponentMenu("redd096/Main/MonoBehaviours/Limits Manager")]
     public class LimitsManager : MonoBehaviour
     {
-        [Header("If there aren't 4 walls, regen on play")]
-        [SerializeField] bool canRegenOnPlay = true;
-        [SerializeField] List<Transform> walls = new List<Transform>();
+        [Header("If enabled, continue update with screen size")]
+        [Tooltip("If this isn't enabled, you can generate walls in editor with a button in inspector, or in awake with this set at true")][SerializeField] bool regenOnAwake;
+        [Tooltip("Use 3d (XZ and BoxCollider) or 2d (XY and BoxCollider2D)")][SerializeField] bool is3D;
 
         Camera cam;
-
         float width;
         float height;
 
-        void Start()
+        private void Awake()
         {
-            //do only if can regen on play
-            if (canRegenOnPlay == false)
-                return;
-
-            cam = Camera.main;
-
-            //if there aren't 4 walls, destroy everything and recreate
-            if (walls == null || walls.Count < 4)
-            {
-                DestroyLimits();
-                CreateLimits();
-            }
+            //regen on awake
+            if (regenOnAwake)
+                GenerateLimits();
         }
 
-        void Update()
+        private void Update()
         {
-            //if there aren't 4 walls, don't resize
-            if (walls == null || walls.Count < 4)
+            //if there aren't 4 limits, don't resize
+            if (transform.childCount < 4)
                 return;
 
-            //if different size, reset limits
+            //if different screen size, resize limits
             if (Screen.width != width || Screen.height != height)
             {
                 width = Screen.width;
                 height = Screen.height;
 
-                SetLimits();
+                SetSize();
             }
         }
 
-        #region public API
-
-        public void RegenLimits()
+        [Button]
+        void GenerateLimits()
         {
             cam = Camera.main;
 
-            //recreate from zero
-            DestroyLimits();
-            CreateLimits();
-            SetLimits();
+            DestroyPrevious();
+            CreateNew();
+            SetSize();
         }
 
-        public void DestroyLimits()
+        #region private API
+
+        /// <summary>
+        /// Remove every child
+        /// </summary>
+        void DestroyPrevious()
         {
-            //remove every child
-            foreach (Transform child in transform)
+            for (int i = transform.childCount - 1; i >= 0; i--)
             {
-#if UNITY_EDITOR
-                EditorApplication.delayCall += () => DestroyImmediate(child.gameObject);
-#else
-                Destroy(child.gameObject);
-#endif
+                if (Application.isPlaying)
+                    Destroy(transform.GetChild(i).gameObject);
+                else
+                    DestroyImmediate(transform.GetChild(i).gameObject);
             }
-
-            //reset list
-            walls = new List<Transform>();
         }
 
-        #endregion
-
-        void CreateLimits()
+        /// <summary>
+        /// Create new objects
+        /// </summary>
+        void CreateNew()
         {
-            //then create walls
             for (int i = 0; i < 4; i++)
             {
-                GameObject wall = new GameObject("Wall", typeof(BoxCollider2D), typeof(SpriteRenderer));
-                wall.transform.SetParent(transform);
-
-                walls.Add(wall.transform);
+                GameObject obj = is3D ? GameObject.CreatePrimitive(PrimitiveType.Cube) : new GameObject("Limit", typeof(BoxCollider2D));
+                obj.name = "Limit";
+                obj.transform.SetParent(transform);
             }
         }
 
-        void SetLimits()
+        /// <summary>
+        /// Set size for every limit
+        /// </summary>
+        void SetSize()
         {
-            //get size of the walls
+            Transform[] limits = new Transform[4];
+            for (int i = 0; i < 4; i++)
+                limits[i] = transform.GetChild(i);
+
+            GetViewportToWorldSize(out Vector3 leftDown, out Vector3 rightUp, out Vector3 center, out Vector3 size, out Vector3 halfSize);
+            if (is3D == false)
+                size = new Vector3(size.x, size.y, 1);
+
+            //right
+            limits[0].position = is3D ? new Vector3(rightUp.x + halfSize.x, center.y, center.z) : new Vector3(rightUp.x + halfSize.x, center.y, 0);
+            limits[0].localScale = size;
+
+            //left
+            limits[1].position = is3D ? new Vector3(leftDown.x - halfSize.x, center.y, center.z) : new Vector3(leftDown.x - halfSize.x, center.y, 0);
+            limits[1].localScale = size;
+
+            //up
+            limits[2].position = is3D ? new Vector3(center.x, center.y, rightUp.z + halfSize.z) : new Vector3(center.x, rightUp.y + halfSize.y, 0);
+            limits[2].localScale = size;
+
+            //down
+            limits[3].position = is3D ? new Vector3(center.x, center.y, leftDown.z - halfSize.z) : new Vector3(center.x, leftDown.y - halfSize.y, 0);
+            limits[3].localScale = size;
+        }
+
+        /// <summary>
+        /// Calculate points of the screen in world space
+        /// </summary>
+        /// <param name="leftDown"></param>
+        /// <param name="rightUp"></param>
+        /// <param name="center"></param>
+        /// <param name="size"></param>
+        /// <param name="halfSize"></param>
+        void GetViewportToWorldSize(out Vector3 leftDown, out Vector3 rightUp, out Vector3 center, out Vector3 size, out Vector3 halfSize)
+        {
             float depthScreen = cam.WorldToViewportPoint(transform.position).z;
-            Vector3 size = GetScale(depthScreen);
 
-            float movementX = size.x / 2;
-            float movementY = size.y / 2;
+            leftDown = cam.ViewportToWorldPoint(new Vector3(0, 0, depthScreen));
+            rightUp = cam.ViewportToWorldPoint(new Vector3(1, 1, depthScreen));
 
-            CreateWall(walls[0], new Vector3(1, 0.5f, depthScreen), size, new Vector3(movementX, 0, 0));        //right
-            CreateWall(walls[1], new Vector3(0, 0.5f, depthScreen), size, new Vector3(-movementX, 0, 0));       //left
-            CreateWall(walls[2], new Vector3(0.5f, 1, depthScreen), size, new Vector3(0, movementY, 0));        //up
-            CreateWall(walls[3], new Vector3(0.5f, 0, depthScreen), size, new Vector3(0, -movementY, 0));       //down
-        }
+            size = rightUp - leftDown;
+            halfSize = size * 0.5f;
 
-        #region set limits
-
-        Vector3 GetScale(float depth)
-        {
-            //get size for the wall from the screen width and height
-            Vector3 left = cam.ViewportToWorldPoint(new Vector3(0, 0, depth));
-            Vector3 right = cam.ViewportToWorldPoint(new Vector3(1.5f, 1.5f, depth));
-
-            Vector3 size = right - left;
-
-            return new Vector3(size.x, size.y, 1);
-        }
-
-        void CreateWall(Transform wall, Vector3 viewportPoint, Vector3 size, Vector3 movement)
-        {
-            //move and set size
-            wall.position = cam.ViewportToWorldPoint(viewportPoint);
-            wall.localScale = size;
-            wall.position += movement;
+            center = rightUp - halfSize;
         }
 
         #endregion
