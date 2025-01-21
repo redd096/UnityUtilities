4407f39ea6fe447765c0229f4874ec31a3f50326
redd096 <martin.babino96@gmail.com>
2025-01-21 15:04:14 +0100

    fix LayoutElementWithlimits

--- Packages/com.redd096.redd096-utilities/Scripts/UIControl/CustomComponents/LayoutElement/LayoutElementWithLimits.cs
+++ Packages/com.redd096.redd096-utilities/Scripts/UIControl/CustomComponents/LayoutElement/LayoutElementWithLimits.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Pool;
 #if UNITY_EDITOR
 using UnityEditor;
 using UnityEditor.UI;
@@ -19,6 +20,9 @@ namespace redd096.UIControl
         [SerializeField] private float m_MaxWidth = -1;
         [SerializeField] private float m_MaxHeight = -1;
 
+        private RectTransform m_RectTransform;
+        private RectTransform rect { get { if (m_RectTransform == null) m_RectTransform = transform as RectTransform; return m_RectTransform; } }
+
         public virtual float maxWidth { get { return m_MaxWidth; } set { if (/*SetPropertyUtility.*/SetStruct(ref m_MaxWidth, value)) SetDirty(); } }
         public virtual float maxHeight { get { return m_MaxHeight; } set { if (/*SetPropertyUtility.*/SetStruct(ref m_MaxHeight, value)) SetDirty(); } }
 
@@ -27,14 +31,87 @@ namespace redd096.UIControl
         {
             if (EqualityComparer<T>.Default.Equals(currentValue, newValue))
                 return false;
-
+            
             currentValue = newValue;
             return true;
         }
 
-        public override float preferredWidth { get => Mathf.Min(base.preferredWidth, maxWidth); set => base.preferredWidth = Mathf.Min(value, maxWidth); }
+        public override float preferredWidth { get => GetMinValue(base.preferredWidth, maxWidth, 0); set => base.preferredWidth = Mathf.Min(value, maxWidth); }
 
-        public override float preferredHeight { get => Mathf.Min(base.preferredHeight, maxHeight); set => base.preferredHeight = Mathf.Min(value, maxHeight); }
+        public override float preferredHeight { get => GetMinValue(base.preferredHeight, maxHeight, 1); set => base.preferredHeight = Mathf.Min(value, maxHeight); }
+
+        #region fix MaxValue enabled and PreferredValue not
+
+        private float GetMinValue(float preferredValue, float maxValue, int axis)
+        {
+            //if preferred isn't enabled but max is
+            if (preferredValue < 0 && maxValue >= 0)
+            {
+                //calculate preferred without this layout element
+                ILayoutElement dummy;
+                if (axis == 0)
+                    preferredValue = Mathf.Max(GetLayoutProperty(rect, e => e.minWidth, 0, out dummy), GetLayoutProperty(rect, e => e.preferredWidth, 0, out dummy));
+                else
+                    preferredValue = Mathf.Max(GetLayoutProperty(rect, e => e.minHeight, 0, out dummy), GetLayoutProperty(rect, e => e.preferredHeight, 0, out dummy));
+            }
+
+            return Mathf.Min(preferredValue, maxValue);
+        }
+
+        //copy-paste from LayoutUtility.GetLayoutProperty in UnityEngine.UI.
+        //But in the for loop now there is a line to ignore this component, and the function isn't static
+        public float GetLayoutProperty(RectTransform rect, System.Func<ILayoutElement, float> property, float defaultValue, out ILayoutElement source)
+        {
+            source = null;
+            if (rect == null)
+                return 0;
+            float min = defaultValue;
+            int maxPriority = System.Int32.MinValue;
+            var components = ListPool<Component>.Get();
+            rect.GetComponents(typeof(ILayoutElement), components);
+
+            var componentsCount = components.Count;
+            for (int i = 0; i < componentsCount; i++)
+            {
+                var layoutComp = components[i] as ILayoutElement;
+                if (layoutComp is Behaviour && !((Behaviour)layoutComp).isActiveAndEnabled)
+                    continue;
+
+                //THIS - IGNORE THIS COMPONENT
+                if (layoutComp is LayoutElementWithLimits compToIgnore && compToIgnore == this)
+                    continue;
+
+                int priority = layoutComp.layoutPriority;
+                // If this layout components has lower priority than a previously used, ignore it.
+                if (priority < maxPriority)
+                    continue;
+                float prop = property(layoutComp);
+                // If this layout property is set to a negative value, it means it should be ignored.
+                if (prop < 0)
+                    continue;
+
+                // If this layout component has higher priority than all previous ones,
+                // overwrite with this one's value.
+                if (priority > maxPriority)
+                {
+                    min = prop;
+                    maxPriority = priority;
+                    source = layoutComp;
+                }
+                // If the layout component has the same priority as a previously used,
+                // use the largest of the values with the same priority.
+                else if (prop > min)
+                {
+                    min = prop;
+                    source = layoutComp;
+                }
+            }
+
+            ListPool<Component>.Release(components);
+            return min;
+        }
+
+        #endregion
     }
 
     #region custom editor
