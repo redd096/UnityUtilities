c032fdffb2ffda0ec274d4f2054e516d4ea9e461
redd096 <martin.babino96@gmail.com>
2025-12-23 17:39:58 +0100

    more snapping utilities

--- Packages/com.redd096.redd096-utilities/Scripts/UIControl/ScrollSnappingUtility.cs
+++ Packages/com.redd096.redd096-utilities/Scripts/UIControl/ScrollSnappingUtility.cs
@@ -11,28 +11,101 @@ namespace redd096.UIControl
         private static Dictionary<ScrollRect, Coroutine> snapCoroutines = new Dictionary<ScrollRect, Coroutine>();
 
         /// <summary>
-        /// This will add an EventTrigger to your ScrollRect and will call OnBeginDrag and OnEndDrag
+        /// This will add an EventTrigger to your ScrollRect and will register to OnBeginDrag and OnEndDrag, to stop and start snapping
         /// </summary>
         /// <param name="scrollRect"></param>
         /// <param name="delayBeforeSnap">Delay after user EndDrag before start Snapping</param>
         /// <param name="snapSpeed"></param>
-        public static void SetSnapping(ScrollRect scrollRect, float delayBeforeSnap = 0f, float snapSpeed = 10f)
+        /// <param name="onBeginDrag">When user begin drag the scrollrect</param>
+        /// <param name="onEndDrag">When user stop drag the scrollrect (before start snapping)</param>
+        /// <param name="onStartSnap">When start the snapping coroutine (after delayBeforeSnap)</param>
+        /// <param name="onUpdateSnap">While snapping (it is called only if child is NOT null)</param>
+        /// <param name="onCompleteSnap">When the snapping coroutine is completed</param>
+        public static void SetSnapping(
+            ScrollRect scrollRect,
+            float delayBeforeSnap = 0f,
+            float snapSpeed = 10f,
+            System.Action<ScrollRect> onBeginDrag = null,
+            System.Action<ScrollRect> onEndDrag = null,
+            System.Action<ScrollRect, Transform> onStartSnap = null,
+            System.Action<ScrollRect, Transform> onUpdateSnap = null,
+            System.Action<ScrollRect, Transform> onCompleteSnap = null)
         {
-            //add event trigger
-            EventTrigger eventTrigger = scrollRect.gameObject.AddComponent<EventTrigger>();
-
-            //begin drag
-            EventTrigger.Entry beginDrag = new EventTrigger.Entry { eventID = EventTriggerType.BeginDrag };
-            beginDrag.callback.AddListener(eventData => OnBeginDrag(scrollRect));
-            eventTrigger.triggers.Add(beginDrag);
-
-            //end drag
-            EventTrigger.Entry endDrag = new EventTrigger.Entry { eventID = EventTriggerType.EndDrag };
-            endDrag.callback.AddListener(eventData => OnEndDrag(scrollRect, delayBeforeSnap, snapSpeed));
-            eventTrigger.triggers.Add(endDrag);
+            SetSnapping(scrollRect, out _, out _, out _, delayBeforeSnap, snapSpeed, onBeginDrag, onEndDrag, onStartSnap, onUpdateSnap, onCompleteSnap);
         }
 
-        private static void OnBeginDrag(ScrollRect scrollRect)
+        /// <summary>
+        /// This will add an EventTrigger to your ScrollRect and will register to OnBeginDrag and OnEndDrag, to stop and start snapping
+        /// </summary>
+        /// <param name="scrollRect"></param>
+        /// <param name="eventTrigger">Added EventTrigger</param>
+        /// <param name="beginDragEntry">EventTrigger added to reigster to OnBeginDrag</param>
+        /// <param name="endDragEntry">EventTrigger added to reigster to OnEndDrag</param>
+        /// <param name="delayBeforeSnap">Delay after user EndDrag before start Snapping</param>
+        /// <param name="snapSpeed"></param>
+        /// <param name="onBeginDrag">When user begin drag the scrollrect</param>
+        /// <param name="onEndDrag">When user stop drag the scrollrect (before start snapping)</param>
+        /// <param name="onStartSnap">When start the snapping coroutine (after delayBeforeSnap)</param>
+        /// <param name="onUpdateSnap">While snapping (it is called only if child is NOT null)</param>
+        /// <param name="onCompleteSnap">When the snapping coroutine is completed</param>
+        public static void SetSnapping(
+            ScrollRect scrollRect,
+            out EventTrigger eventTrigger,
+            out EventTrigger.Entry beginDragEntry,
+            out EventTrigger.Entry endDragEntry,
+            float delayBeforeSnap = 0f,
+            float snapSpeed = 10f,
+            System.Action<ScrollRect> onBeginDrag = null,
+            System.Action<ScrollRect> onEndDrag = null,
+            System.Action<ScrollRect, Transform> onStartSnap = null,
+            System.Action<ScrollRect, Transform> onUpdateSnap = null,
+            System.Action<ScrollRect, Transform> onCompleteSnap = null)
+        {
+            //add event trigger
+            eventTrigger = scrollRect.gameObject.AddComponent<EventTrigger>();
+
+            //begin drag
+            beginDragEntry = new EventTrigger.Entry { eventID = EventTriggerType.BeginDrag };
+            beginDragEntry.callback.AddListener(eventData => OnBeginDrag(scrollRect, onBeginDrag));
+            eventTrigger.triggers.Add(beginDragEntry);
+
+            //end drag
+            endDragEntry = new EventTrigger.Entry { eventID = EventTriggerType.EndDrag };
+            endDragEntry.callback.AddListener(eventData => OnEndDrag(scrollRect, delayBeforeSnap, snapSpeed, onEndDrag, onStartSnap, onUpdateSnap, onCompleteSnap));
+            eventTrigger.triggers.Add(endDragEntry);
+        }
+
+        /// <summary>
+        /// This will start the snapping coroutine
+        /// </summary>
+        /// <param name="scrollRect"></param>
+        /// <param name="child">child to snap to</param>
+        /// <param name="snapSpeed"></param>
+        /// <param name="onUpdateSnap">While snapping (it is called only if child is NOT null)</param>
+        /// <param name="onCompleteSnap">When the snapping coroutine is completed</param>
+        public static void SnapToChild(
+            ScrollRect scrollRect,
+            Transform child,
+            float snapSpeed = 10f,
+            System.Action<ScrollRect, Transform> onUpdateSnap = null,
+            System.Action<ScrollRect, Transform> onCompleteSnap = null)
+        {
+            //to be sure, stop coroutine
+            if (snapCoroutines.ContainsKey(scrollRect))
+            {
+                scrollRect.StopCoroutine(snapCoroutines[scrollRect]);
+                snapCoroutines.Remove(scrollRect);
+            }
+
+            //start coroutine
+            snapCoroutines.Add(scrollRect, scrollRect.StartCoroutine(SnapToChildCoroutine(scrollRect, child, snapSpeed, onUpdateSnap, onCompleteSnap)));
+        }
+
+        #region private API
+
+        private static void OnBeginDrag(
+            ScrollRect scrollRect,
+            System.Action<ScrollRect> onBeginDrag)
         {
             //stop coroutine OnBegin Drag
             if (snapCoroutines.ContainsKey(scrollRect))
@@ -40,9 +113,18 @@ namespace redd096.UIControl
                 scrollRect.StopCoroutine(snapCoroutines[scrollRect]);
                 snapCoroutines.Remove(scrollRect);
             }
+
+            onBeginDrag?.Invoke(scrollRect);
         }
 
-        private static void OnEndDrag(ScrollRect scrollRect, float delayBeforeSnap, float snapSpeed)
+        private static void OnEndDrag(
+            ScrollRect scrollRect,
+            float delayBeforeSnap,
+            float snapSpeed,
+            System.Action<ScrollRect> onEndDrag,
+            System.Action<ScrollRect, Transform> onStartSnap,
+            System.Action<ScrollRect, Transform> onUpdateSnap,
+            System.Action<ScrollRect, Transform> onCompleteSnap)
         {
             //just to be sure, stop coroutine
             if (snapCoroutines.ContainsKey(scrollRect))
@@ -51,11 +133,19 @@ namespace redd096.UIControl
                 snapCoroutines.Remove(scrollRect);
             }
 
+            onEndDrag?.Invoke(scrollRect);
+
             //start coroutine OnEnd Drag
-            snapCoroutines.Add(scrollRect, scrollRect.StartCoroutine(SnapCoroutine(scrollRect, delayBeforeSnap, snapSpeed)));
+            snapCoroutines.Add(scrollRect, scrollRect.StartCoroutine(SnapCoroutine(scrollRect, delayBeforeSnap, snapSpeed, onStartSnap, onUpdateSnap, onCompleteSnap)));
         }
 
-        private static IEnumerator SnapCoroutine(ScrollRect scrollRect, float delayBeforeSnap, float snapSpeed)
+        private static IEnumerator SnapCoroutine(
+            ScrollRect scrollRect,
+            float delayBeforeSnap,
+            float snapSpeed,
+            System.Action<ScrollRect, Transform> onStartSnap,
+            System.Action<ScrollRect, Transform> onUpdateSnap,
+            System.Action<ScrollRect, Transform> onCompleteSnap)
         {
             //if user doesn't scroll for few seconds, start snapping
             yield return new WaitForSeconds(delayBeforeSnap);
@@ -66,7 +156,7 @@ namespace redd096.UIControl
 
             //find nearest child
             float closestDistance = float.MaxValue;
-            RectTransform closestChild = null;
+            Transform closestChild = null;
             foreach (RectTransform child in container)
             {
                 Vector3 childCenter = child.TransformPoint(child.rect.center);
@@ -79,21 +169,44 @@ namespace redd096.UIControl
                 }
             }
 
-            if (closestChild)
+            //and snap to it
+            onStartSnap?.Invoke(scrollRect, closestChild);
+            yield return SnapToChildCoroutine(scrollRect, closestChild, snapSpeed, onUpdateSnap, onCompleteSnap);
+        }
+
+        private static IEnumerator SnapToChildCoroutine(
+            ScrollRect scrollRect,
+            Transform child,
+            float snapSpeed,
+            System.Action<ScrollRect, Transform> onUpdateSnap,
+            System.Action<ScrollRect, Transform> onCompleteSnap)
+        {
+            //calculate center
+            Transform container = scrollRect.content;
+            RectTransform scrollRt = scrollRect.GetComponent<RectTransform>();
+            Vector3 center = scrollRt.TransformPoint(scrollRt.rect.center);
+
+            if (child)
             {
                 //calculate distance
                 Vector3 targetPosition = container.localPosition;
-                Vector3 difference = container.InverseTransformPoint(closestChild.position) - container.InverseTransformPoint(center);
+                Vector3 difference = container.InverseTransformPoint(child.position) - container.InverseTransformPoint(center);
                 targetPosition.x -= difference.x;
 
                 //do snapping
                 while (Vector3.Distance(container.localPosition, targetPosition) > 0.1f)
                 {
                     container.localPosition = Vector3.Lerp(container.localPosition, targetPosition, Time.deltaTime * snapSpeed);
+                    onUpdateSnap?.Invoke(scrollRect, child);
                     yield return null;
                 }
+
                 container.localPosition = targetPosition;
             }
+
+            onCompleteSnap?.Invoke(scrollRect, child);
         }
+
+        #endregion
     }
 }
